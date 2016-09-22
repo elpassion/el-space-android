4766e1ed89c178639710b399343954c31c3d6c8e
Karol Kowalski <ordon1313@gmail.com>
2016-09-22 15:41:30 +0200

    Should start ProjectChooseActivity

---
+++ app/src/androidTest/kotlin/pl/elpassion/project/choose/ProjectChooseActivityTest.kt
@@ -0,0 +1,17 @@
+package pl.elpassion.project.choose
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class ProjectChooseActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<ProjectChooseActivity>(ProjectChooseActivity::class.java)
+
+    @Test
+    fun shouldStartActivity() {
+
+    }
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/project/choose/ProjectChooseActivity.kt
@@ -0,0 +1,7 @@
+package pl.elpassion.project.choose
+
+import android.support.v7.app.AppCompatActivity
+
+class ProjectChooseActivity : AppCompatActivity() {
+
+}

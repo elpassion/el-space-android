625e0cd13fc1364722267b997a1cca5d732a5b00
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-22 19:58:08 +0200

    Correctly fire ReportListActivity

---
+++ app/src/androidTest/kotlin/pl/elpassion/report/list/ReportListActivityTest.kt
@@ -0,0 +1,19 @@
+package pl.elpassion.report.list
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class ReportListActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<ReportListActivity>(ReportListActivity::class.java)
+
+
+    @Test
+    fun shouldCorrectlyFireReportListActivity() {
+
+    }
+
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/report/list/ReportListActivity.kt
@@ -0,0 +1,7 @@
+package pl.elpassion.report.list
+
+import android.support.v7.app.AppCompatActivity
+
+class ReportListActivity :AppCompatActivity(){
+
+}

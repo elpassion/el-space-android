38cf20121585852abca9975f9313ac2a511e6b35
Karol Kowalski <ordon1313@gmail.com>
2016-09-22 18:04:23 +0200

    Start AddReportActivity

---
+++ app/src/androidTest/kotlin/pl/elpassion/report/add/AddReportActivityTest.kt
@@ -0,0 +1,16 @@
+package pl.elpassion.report.add
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class AddReportActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<AddReportActivity>(AddReportActivity::class.java)
+
+    @Test
+    fun shouldStart() {
+    }
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/report/add/AddReportActivity.kt
@@ -0,0 +1,6 @@
+package pl.elpassion.report.add
+
+import android.support.v7.app.AppCompatActivity
+
+class AddReportActivity : AppCompatActivity() {
+}

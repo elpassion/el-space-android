e0349c054ae959d5ffd33664588d961487dfed68
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-22 12:15:18 +0200

    Remove java

--- app/src/androidTest/kotlin/pl/elpassion/ExampleInstrumentedTest.java
+++ app/src/androidTest/kotlin/pl/elpassion/ExampleInstrumentedTest.java
@@ -9,12 +9,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {

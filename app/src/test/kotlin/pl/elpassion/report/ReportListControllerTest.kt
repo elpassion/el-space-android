617b9c0337bd3b7866876c3a0354d7e9fd58f8d1
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-22 14:03:06 +0200

    [refactoring] Extract duplicated code to separate methods + remove joda-time from classpath

--- app/src/test/kotlin/pl/elpassion/report/ReportListControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/report/ReportListControllerTest.kt
@@ -4,39 +4,45 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import org.joda.time.DateTime
 import org.junit.Test
 import pl.elpassion.common.Provider
 import java.util.*
 
 class ReportListControllerTest {
 
+    val api = mock<ReportList.Api>()
+    val view = mock<ReportList.View>()
+    val controller = ActivitiesController(api, view)
+
     @Test
     fun shouldDisplay31DaysWithoutReportsIfIsOctoberAndApiReturnsEmptyListOnCreate() {
-        CurrentTimeProvider.override = { dateInMillis(month = 10) }
-        val days = ArrayList<Day>()
-        (1..31).forEach { days.add(Day(it)) }
-        val api = mock<ReportList.Api>()
-        whenever(api.getActivities()).thenReturn(emptyList())
-        val view = mock<ReportList.View>()
-        val controller = ActivitiesController(api, view)
-        controller.onCreate()
-
-        verify(view, times(1)).showActivities(days)
+        verifyIfShowCorrectListForGivenParams(
+                apiReturnValue = emptyList<Report>(),
+                month = 10,
+                daysInMonth = 31)
     }
 
     @Test
     fun shouldDisplay30DaysWithoutReportsIfIsNovemberAndApiReturnsEmptyListOnCreate() {
-        CurrentTimeProvider.override = { dateInMillis(month = 11) }
-        val days = ArrayList<Day>()
-        (1..30).forEach { days.add(Day(it)) }
-        val api = mock<ReportList.Api>()
-        whenever(api.getActivities()).thenReturn(emptyList())
-        val view = mock<ReportList.View>()
-        val controller = ActivitiesController(api, view)
-        controller.onCreate()
+        verifyIfShowCorrectListForGivenParams(
+                apiReturnValue = emptyList(),
+                month = 11,
+                daysInMonth = 30
+        )
+    }
 
+    private fun verifyIfShowCorrectListForGivenParams(apiReturnValue: List<Report>, daysInMonth: Int, month: Int) {
+        CurrentTimeProvider.override = { dateInMillis(month = month) }
+        val days = ArrayList<Day>().apply {
+            (1..daysInMonth).forEach { add(Day(it)) }
+        }
+        stubApiToReturn(apiReturnValue)
+        controller.onCreate()
         verify(view, times(1)).showActivities(days)
+    }
+
+    private fun stubApiToReturn(list: List<Report>) {
+        whenever(api.getActivities()).thenReturn(list)
     }
 
     private fun dateInMillis(year: Int = 2016, month: Int = 6, day: Int = 1) = Calendar.getInstance().run {
@@ -71,6 +77,8 @@ class ActivitiesController(val api: ReportList.Api, val view: ReportList.View) {
         view.showActivities(days)
     }
 
-    private fun daysForCurrentMonth() = DateTime(CurrentTimeProvider.get()).toGregorianCalendar().getActualMaximum(Calendar.DAY_OF_MONTH)
-
+    private fun daysForCurrentMonth() = Calendar.getInstance().run {
+        time = Date(CurrentTimeProvider.get())
+        getActualMaximum(Calendar.DAY_OF_MONTH)
+    }
 }

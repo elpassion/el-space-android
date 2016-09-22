45c83e70dc3cae022247723f52b7979ba16b34d7
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-22 13:41:16 +0200

    Display 30 days without reports if api returns empty list and is november on create

--- app/src/test/kotlin/pl/elpassion/report/ReportListControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/report/ReportListControllerTest.kt
@@ -25,7 +25,24 @@ class ReportListControllerTest {
         verify(view, times(1)).showActivities(days)
     }
 
-    private fun dateInMillis(year: Int = 2016, month: Int = 6, day: Int = 1): Long = DateTime(year, month, day, 0, 0).millis
+    @Test
+    fun shouldDisplay30DaysWithoutReportsIfIsNovemberAndApiReturnsEmptyListOnCreate() {
+        CurrentTimeProvider.override = { dateInMillis(month = 11) }
+        val days = ArrayList<Day>()
+        (1..30).forEach { days.add(Day(it)) }
+        val api = mock<ReportList.Api>()
+        whenever(api.getActivities()).thenReturn(emptyList())
+        val view = mock<ReportList.View>()
+        val controller = ActivitiesController(api, view)
+        controller.onCreate()
+
+        verify(view, times(1)).showActivities(days)
+    }
+
+    private fun dateInMillis(year: Int = 2016, month: Int = 6, day: Int = 1) = Calendar.getInstance().run {
+        set(year, month - 1, day)
+        timeInMillis
+    }
 
 }
 
@@ -50,7 +67,10 @@ class Report()
 class ActivitiesController(val api: ReportList.Api, val view: ReportList.View) {
     fun onCreate() {
         val days = ArrayList<Day>()
-        (1..31).forEach { days.add(Day(it)) }
+        (1..daysForCurrentMonth()).forEach { days.add(Day(it)) }
         view.showActivities(days)
     }
+
+    private fun daysForCurrentMonth() = DateTime(CurrentTimeProvider.get()).toGregorianCalendar().getActualMaximum(Calendar.DAY_OF_MONTH)
+
 }

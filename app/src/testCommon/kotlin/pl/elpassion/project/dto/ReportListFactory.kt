fabdb1e1b432e55f950a41d5e976d8c4cf0d71f4
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-23 22:09:03 +0200

    [refactoring] Remove unused code

--- app/src/testCommon/kotlin/pl/elpassion/project/dto/ReportListFactory.kt
+++ app/src/testCommon/kotlin/pl/elpassion/project/dto/ReportListFactory.kt
@@ -1,10 +1,6 @@
 package pl.elpassion.project.dto
 
-import pl.elpassion.report.list.Day
 import pl.elpassion.report.list.Report
 
-fun newDay(dayNumber: Int, reports: List<Report> = emptyList(), hasPassed: Boolean = false) =
-        Day(dayNumber = dayNumber, reports = reports, hasPassed = hasPassed, isWeekendDay = false)
-
-fun newReport(year: Int = 2016, month: Int = 6, day: Int = 1, reportedHours: Double = 4.0, projectId: Long = 1, projectName: String = "Project", description : String = "description") =
+fun newReport(year: Int = 2016, month: Int = 6, day: Int = 1, reportedHours: Double = 4.0, projectId: Long = 1, projectName: String = "Project", description: String = "description") =
         Report(year = year, month = month, day = day, reportedHours = reportedHours, projectId = projectId, projectName = projectName, description = description)

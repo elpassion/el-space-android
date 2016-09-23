8df9e211e352bee607dcf3ebf2fa09f11ebe8245
Karol Kowalski <ordon1313@gmail.com>
2016-09-23 10:28:44 +0200

    Start with first project selected

--- app/src/androidTest/kotlin/pl/elpassion/report/add/AddReportActivityTest.kt
+++ app/src/androidTest/kotlin/pl/elpassion/report/add/AddReportActivityTest.kt
@@ -1,16 +1,34 @@
 package pl.elpassion.report.add
 
 import android.support.test.rule.ActivityTestRule
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onText
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Rule
 import org.junit.Test
+import pl.elpassion.project.choose.ProjectChoose
+import pl.elpassion.project.dto.Project
+import pl.elpassion.project.dto.newProject
+import pl.elpassion.startActivity
 
 class AddReportActivityTest {
 
+    val repository = mock<ProjectChoose.Repository>()
+
     @JvmField @Rule
-    val rule = ActivityTestRule<AddReportActivity>(AddReportActivity::class.java)
+    val rule = ActivityTestRule<AddReportActivity>(AddReportActivity::class.java, false, false)
 
     @Test
-    fun shouldStart() {
+    fun shouldStartWithFirstProjectSelected() {
+        stubRepository(listOf(newProject()))
+        rule.startActivity()
+        onText("name").isDisplayed()
+    }
+
+    private fun stubRepository(listOf: List<Project>) {
+        whenever(repository.getPossibleProjects()).thenReturn(listOf)
+        ProjectChoose.RepositoryProvider.override = { repository }
     }
 }
 
--- app/src/main/kotlin/pl/elpassion/report/add/AddReportActivity.kt
+++ app/src/main/kotlin/pl/elpassion/report/add/AddReportActivity.kt
@@ -1,6 +1,13 @@
 package pl.elpassion.report.add
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pl.elpassion.R
 
 class AddReportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.report_add_activity)
+    }
 }

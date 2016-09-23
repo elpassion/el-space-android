03ae66ef0751d4efe48f179968db3b1823435e5a
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-23 16:31:35 +0200

    Lol fixes 2

--- app/src/main/kotlin/pl/elpassion/report/list/ReportFromApi.kt
+++ app/src/main/kotlin/pl/elpassion/report/list/ReportFromApi.kt
@@ -6,15 +6,15 @@ import pl.elpassion.common.yearValue
 import pl.elpassion.project.common.Project
 import java.util.*
 
-class ReportFromApi(val createdAt: Date, val value: Double, val projectId: Long, val comment: String) {
+class ReportFromApi(val createdAt: Date, val value: Double?, val projectId: Long?, val comment: String) {
 
     fun toReport(projects: List<Project>): Report {
         return Report(
                 year = createdAt.yearValue(),
                 month = createdAt.monthValue(),
                 day = createdAt.dayValue(),
-                reportedHours = value,
-                projectId = projectId,
+                reportedHours = value ?: 0.0,
+                projectId = projectId ?: -1,
                 projectName = projects.firstOrNull { it.id.equals(projectId.toString()) }?.name ?: "Unknown",
                 description = comment)
     }

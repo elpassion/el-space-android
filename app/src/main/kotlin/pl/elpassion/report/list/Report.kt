0ae2c513e4cdcb134c4ae2e269de95d837525fc4
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-23 11:19:07 +0200

    Added fields to report class

--- app/src/main/kotlin/pl/elpassion/report/list/Report.kt
+++ app/src/main/kotlin/pl/elpassion/report/list/Report.kt
@@ -1,3 +1,3 @@
 package pl.elpassion.report.list
 
-data class Report(val year: Int, val month: Int, val day: Int, val reportedHours: Double)
+data class Report(val year: Int, val month: Int, val day: Int, val reportedHours: Double, val projectName: String, val projectId: Long, val description: String)

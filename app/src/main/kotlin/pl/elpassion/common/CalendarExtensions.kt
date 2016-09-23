9ff907bd0576a01ecd089fb10ec4dbbff0765ee5
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-23 11:09:16 +0200

    [refactoring] added type to get rid off error

--- app/src/main/kotlin/pl/elpassion/common/CalendarExtensions.kt
+++ app/src/main/kotlin/pl/elpassion/common/CalendarExtensions.kt
@@ -8,4 +8,4 @@ fun Calendar.changeToNextMonth() = add(Calendar.MONTH, 1)
 fun Calendar.isNotAfter(sth: Any) = !after(sth)
 fun getCurrentTimeCalendar(): Calendar = Calendar.getInstance().apply { time = Date(CurrentTimeProvider.get()) }
 fun getTimeFrom(year: Int, month: Int, day: Int): Calendar = Calendar.getInstance().apply { set(year, month, day) }
-fun Calendar.getFullMonthName() = SimpleDateFormat("MMMM", Locale.UK).format(this.time)
+fun Calendar.getFullMonthName(): String = SimpleDateFormat("MMMM", Locale.UK).format(this.time)

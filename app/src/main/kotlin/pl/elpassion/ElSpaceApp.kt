40ef777b4a9ee8eb2d8c78497926c80b87436c36
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-25 23:54:28 +0200

    Added crashlytic to project

--- app/src/main/kotlin/pl/elpassion/ElSpaceApp.kt
+++ app/src/main/kotlin/pl/elpassion/ElSpaceApp.kt
@@ -1,6 +1,8 @@
 package pl.elpassion
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import pl.elpassion.common.ContextProvider
 import pl.elpassion.common.SchedulersTransformerProvider
 
@@ -8,6 +10,7 @@ class ElSpaceApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         ContextProvider.override = { applicationContext }
         SchedulersTransformerProvider.override = { SchedulersTransformerProvider.AndroidSchedulersTransformer }
     }
---
+++ scripts/deploy_crashlytics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+echo $DIR
+RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
+------
+%B" -n 1 $TRAVIS_COMMIT)
+echo "$RELEASE_NOTES" > $DIR/../crashlytics_release_notes.txt
+./gradlew :demo:crashlyticsUploadDistributionDebug

561b76984ef5bd952e482eb2b39b619ca81f8980
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-25 20:10:46 +0200

    LoginActivity init test

---
+++ app/src/androidTest/kotlin/pl/elpassion/login/LoginActivityTest.kt
@@ -0,0 +1,17 @@
+package pl.elpassion.login
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class LoginActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+
+    @Test
+    fun initTest() {
+
+    }
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package pl.elpassion.login
+
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+
+}

d27ef3fb5642dfe02d5ad66d484ad4cf5d41f030
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-25 18:29:02 +0200

    Remove redundant file

--- app/src/main/kotlin/pl/elpassion/common/DateExtensions.kt
+++
@@ -1,7 +0,0 @@
-package pl.elpassion.common
-
-import java.util.*
-
-fun Date.yearValue() = Calendar.getInstance().apply { time = this@yearValue }.get(Calendar.YEAR)
-fun Date.monthValue() = Calendar.getInstance().apply { time = this@monthValue }.get(Calendar.MONTH) + 1
-fun Date.dayValue() = Calendar.getInstance().apply { time = this@dayValue }.get(Calendar.DAY_OF_MONTH)

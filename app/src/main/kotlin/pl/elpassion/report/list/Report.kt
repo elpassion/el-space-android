4b386862117ca96ff6085900b947317733cce776
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-25 18:56:55 +0200

    [refactoring] Format classes

--- app/src/main/kotlin/pl/elpassion/report/list/Report.kt
+++ app/src/main/kotlin/pl/elpassion/report/list/Report.kt
@@ -1,3 +1,10 @@
 package pl.elpassion.report.list
 
-data class Report(val year: Int, val month: Int, val day: Int, val reportedHours: Double, val projectName: String, val projectId: Long, val description: String)
+data class Report(
+        val year: Int,
+        val month: Int,
+        val day: Int,
+        val reportedHours: Double,
+        val projectName: String,
+        val projectId: Long,
+        val description: String)

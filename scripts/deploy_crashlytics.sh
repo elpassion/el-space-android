0fcc0397d65867005cf6f1e724e58b19acfbc931
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-10-01 21:09:05 +0200

    Change deploy_crashlytics script to deploy only master branch

--- scripts/deploy_crashlytics.sh
+++ scripts/deploy_crashlytics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+if [$TRAVIS_BRANCH -eq 'master'];
+then
 DIR=$(dirname $0)
 echo $DIR
 RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
@@ -7,3 +8,4 @@ RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
 %B" -n 1 $TRAVIS_COMMIT)
 echo "$RELEASE_NOTES" > $DIR/../crashlytics_release_notes.txt
 ./gradlew :app:crashlyticsUploadDistributionDebug
+fi

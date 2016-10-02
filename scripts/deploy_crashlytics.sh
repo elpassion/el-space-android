f72c1ccbc9da469f1d49150696f36766dd21d674
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-10-02 13:36:05 +0200

    Move deploying logic to travis.yml

--- scripts/deploy_crashlytics.sh
+++ scripts/deploy_crashlytics.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if ["$TRAVIS_BRANCH" == "master"];
-then
 DIR=$(dirname $0)
 echo $DIR
 RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
@@ -8,4 +6,3 @@ RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
 %B" -n 1 $TRAVIS_COMMIT)
 echo "$RELEASE_NOTES" > $DIR/../crashlytics_release_notes.txt
 ./gradlew :app:crashlyticsUploadDistributionDebug
-fi

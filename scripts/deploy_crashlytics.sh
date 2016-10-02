cb883fabe78241ed2ad25171726d9236327f5cfb
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-10-02 14:58:52 +0200

    deploy script fix

--- scripts/deploy_crashlytics.sh
+++ scripts/deploy_crashlytics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 DIR=$(dirname $0)
 echo $DIR
 RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
@@ -7,4 +7,4 @@ RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
 %B" -n 1 $TRAVIS_COMMIT)
 echo "$RELEASE_NOTES" > $DIR/../crashlytics_release_notes.txt
 ./gradlew :app:crashlyticsUploadDistributionDebug
-exit
+fi

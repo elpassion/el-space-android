e2dd6ee45926408ef35e0ad3a55f5bcee16897bf
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-11-28 17:43:57 +0100

    [#1] ReportEditActivity init test

---
+++ app/src/androidTest/kotlin/pl/elpassion/report/edit/ReportEditActivityTest.kt
@@ -0,0 +1,18 @@
+package pl.elpassion.report.edit
+
+import org.junit.Rule
+import org.junit.Test
+import pl.elpassion.common.rule
+
+class ReportEditActivityTest {
+
+    @JvmField @Rule
+    val rule = rule<ReportEditActivity>()
+
+    @Test
+    fun initTest() {
+
+    }
+
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/report/edit/ReportEditActivity.kt
@@ -0,0 +1,5 @@
+package pl.elpassion.report.edit
+
+import android.support.v7.app.AppCompatActivity
+
+class ReportEditActivity : AppCompatActivity()

f4f328348b79d4305054b4c9406e96442f4d1f04
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-11-28 14:07:51 +0100

    [#1] call api with correct data on save report

--- app/src/test/kotlin/pl/elpassion/report/edit/ReportEditControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/report/edit/ReportEditControllerTest.kt
@@ -10,7 +10,8 @@ import pl.elpassion.report.Report
 class ReportEditControllerTest {
 
     private val view = mock<ReportEdit.View>()
-    private val controller = ReportEditController(view)
+    private val editReportApi = mock<ReportEdit.EditApi>()
+    private val controller = ReportEditController(view, editReportApi)
 
     @Test
     fun shouldShowCorrectReportOnCreate() {
@@ -25,9 +26,16 @@ class ReportEditControllerTest {
         verify(view, times(1)).openChooseProjectScreen()
     }
 
+    @Test
+    fun shouldCallApiWithCorrectDataOnSaveReport() {
+        controller.onCreate(newReport(year = 2017, month = 7, day = 2, id = 2, description = "DESCRIPTION", reportedHours = 4.0, projectId = 2))
+        controller.onSaveReport(hours = 8.0, description = "description")
+        verify(editReportApi, times(1)).editReport(id = 2, date = "2017-07-02", reportedHour = 8.0, description = "description", projectId = 2)
+    }
+
 }
 
-class ReportEditController(val view: ReportEdit.View) {
+class ReportEditController(val view: ReportEdit.View, val editReportApi: ReportEdit.EditApi) {
 
     fun onCreate(report: Report) {
         view.showReport(report)
@@ -36,11 +44,19 @@ class ReportEditController(val view: ReportEdit.View) {
     fun onChooseProject() {
         view.openChooseProjectScreen()
     }
+
+    fun onSaveReport(hours: Double, description: String) {
+        editReportApi.editReport(id = 2, date = "2017-07-02", reportedHour = 8.0, description = "description", projectId = 2 )
+    }
 }
 
 interface ReportEdit {
     interface View {
         fun showReport(report: Report)
         fun openChooseProjectScreen()
+    }
+
+    interface EditApi {
+        fun editReport(id: Int, date: String, reportedHour: Double, description: String, projectId: Int)
     }
 }

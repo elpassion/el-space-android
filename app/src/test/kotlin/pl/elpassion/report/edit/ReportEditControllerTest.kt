b33f0cd3283aa5dc8179fc7520f7722522cae2a7
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-11-28 12:41:03 +0100

    [#1] show correct report on create

--- app/src/test/kotlin/pl/elpassion/report/edit/ReportEditControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/report/edit/ReportEditControllerTest.kt
@@ -1,14 +1,33 @@
 package pl.elpassion.report.edit
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
+import pl.elpassion.project.dto.newReport
+import pl.elpassion.report.Report
 
 class ReportEditControllerTest {
 
     @Test
-    fun initTest() {
-        ReportEditController()
+    fun shouldShowCorrectReportOnCreate() {
+        val view = mock<ReportEdit.View>()
+        val report = newReport()
+        ReportEditController(view).onCreate(report)
+        verify(view, times(1)).showReport(report)
     }
 
 }
 
-class ReportEditController
+class ReportEditController(val view: ReportEdit.View) {
+
+    fun onCreate(report: Report) {
+        view.showReport(report)
+    }
+}
+
+interface ReportEdit {
+    interface View {
+        fun showReport(report: Report)
+    }
+}

39338958c2d7d2e3c79cd7a511f32f8204fbb89d
Dominik <dominik.malantowicz@gmail.com>
2016-12-14 15:10:28 +0100

    [#9] show log to previous debate view if token is provided on create

--- app/src/test/kotlin/pl/elpassion/debate/login/DebateLoginControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/debate/login/DebateLoginControllerTest.kt
@@ -1,14 +1,28 @@
 package pl.elpassion.debate.login
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class DebateLoginControllerTest {
 
     @Test
-    fun initTest() {
-        DebateLoginController()
+    fun shouldShowLogToPreviousDebateViewIfTokenIsProvidedOnCreate() {
+        val view = mock<DebateLogin.View>()
+        DebateLoginController(view).onCreate()
+        verify(view).showLogToPreviousDebateView()
     }
 
 }
 
-class DebateLoginController
+interface DebateLogin {
+    interface View {
+        fun showLogToPreviousDebateView()
+    }
+}
+
+class DebateLoginController(private val view: DebateLogin.View) {
+    fun onCreate() {
+        view.showLogToPreviousDebateView()
+    }
+}

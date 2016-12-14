1dc65962a9af960ea25eed6f02db25523a0b7173
Dominik <dominik.malantowicz@gmail.com>
2016-12-14 15:03:36 +0100

    [#9] DebateLoginController init test

---
+++ app/src/test/kotlin/pl/elpassion/debate/login/DebateLoginControllerTest.kt
@@ -0,0 +1,14 @@
+package pl.elpassion.debate.login
+
+import org.junit.Test
+
+class DebateLoginControllerTest {
+
+    @Test
+    fun initTest() {
+        DebateLoginController()
+    }
+
+}
+
+class DebateLoginController

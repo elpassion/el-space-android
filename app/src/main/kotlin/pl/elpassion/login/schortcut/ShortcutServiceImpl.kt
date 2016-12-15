b3297884b7374fa5716c22ca7e2175a7821739db
Marcin Suska <marcin.suska@elpassion.pl>
2016-12-15 19:44:26 +0100

    Fix: Add action to intent in ShortcutService

--- app/src/main/kotlin/pl/elpassion/login/schortcut/ShortcutServiceImpl.kt
+++ app/src/main/kotlin/pl/elpassion/login/schortcut/ShortcutServiceImpl.kt
@@ -2,6 +2,7 @@ package pl.elpassion.login.schortcut
 
 import android.annotation.TargetApi
 import android.content.Context
+import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
 import android.graphics.drawable.Icon
@@ -25,7 +26,7 @@ class ShortcutServiceImpl(val context: Context) : ShortcutService {
             .setShortLabel(context.getString(R.string.shortcut_report_hub_short))
             .setLongLabel(context.getString(R.string.shortcut_report_hub_long))
             .setIcon(Icon.createWithResource(context, R.mipmap.ic_launcher))
-            .setIntent(ReportAddActivity.intent(context, ""))
+            .setIntent(ReportAddActivity.intent(context, "").apply { action = Intent.ACTION_MAIN })
             .build()
 
 }

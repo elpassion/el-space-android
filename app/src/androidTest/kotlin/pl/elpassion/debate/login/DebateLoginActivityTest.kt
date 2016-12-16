c2efb1f20c95557c2cad13e760ecaa77feab5f3d
Dominik <dominik.malantowicz@gmail.com>
2016-12-16 17:10:35 +0100

    [#9] Have correct debate code input

--- app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
+++ app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
@@ -1,6 +1,13 @@
 package pl.elpassion.debate.login
 
+import android.support.test.espresso.action.ViewActions.replaceText
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withInputType
+import android.text.InputType.TYPE_CLASS_NUMBER
+import android.text.InputType.TYPE_NUMBER_VARIATION_NORMAL
+import com.elpassion.android.commons.espresso.hasText
 import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onId
 import com.elpassion.android.commons.espresso.onText
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +22,20 @@ class DebateLoginActivityTest {
     @Test
     fun shouldHaveWelcomeString() {
         onText(R.string.debate_login_welcome).isDisplayed()
+    }
+
+    @Test
+    fun shouldHaveInstructionsString() {
+        onText(R.string.debate_login_instructions).isDisplayed()
+    }
+
+    @Test
+    fun shouldHaveCorrectDebateCodeInput() {
+        onId(R.id.debateCode)
+                .isDisplayed()
+                .perform(replaceText("123456"))
+                .hasText("12345")
+                .check(matches(withInputType(TYPE_CLASS_NUMBER or TYPE_NUMBER_VARIATION_NORMAL)))
     }
 }
 

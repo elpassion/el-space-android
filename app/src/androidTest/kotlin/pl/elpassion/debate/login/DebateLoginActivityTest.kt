0bee3b53e14ade12e9a13e6d167cd573e9e28640
Dominik <dominik.malantowicz@gmail.com>
2016-12-16 16:30:37 +0100

    [#9] Have welcome string

--- app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
+++ app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
@@ -1,7 +1,10 @@
 package pl.elpassion.debate.login
 
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onText
 import org.junit.Rule
 import org.junit.Test
+import pl.elpassion.R
 import pl.elpassion.common.rule
 
 class DebateLoginActivityTest {
@@ -10,8 +13,8 @@ class DebateLoginActivityTest {
     val rule = rule<DebateLoginActivity>()
 
     @Test
-    fun initTest() {
-
+    fun shouldHaveWelcomeString() {
+        onText(R.string.debate_login_welcome).isDisplayed()
     }
 }
 

281426639d6f4f9cb0232953013760ea7e6e0fed
Dominik <dominik.malantowicz@gmail.com>
2016-12-20 19:20:05 +0100

    [refactoting] auto format

--- app/src/main/kotlin/pl/elpassion/project/ProjectRepositoryImpl.kt
+++ app/src/main/kotlin/pl/elpassion/project/ProjectRepositoryImpl.kt
@@ -7,9 +7,8 @@ import rx.Observable
 class ProjectRepositoryImpl(private val projectListService: ProjectListService,
                             private val cachedProjectRepository: CachedProjectRepository) : ProjectRepository {
 
-    override fun getProjects(): Observable<List<Project>> =
-         when {
-            cachedProjectRepository.hasProjects() -> Observable.just(cachedProjectRepository.getPossibleProjects())
-            else -> projectListService.getProjects()
+    override fun getProjects(): Observable<List<Project>> = when {
+        cachedProjectRepository.hasProjects() -> Observable.just(cachedProjectRepository.getPossibleProjects())
+        else -> projectListService.getProjects()
     }
 }

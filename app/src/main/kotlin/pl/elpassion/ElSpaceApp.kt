23f1dde57709d7b54f1dafd78e900f7eb7726342
ghostbuster91 <kghost0@gmail.com>
2016-12-27 15:53:16 +0100

    Run Crashlytics only for release builds

--- app/src/main/kotlin/pl/elpassion/ElSpaceApp.kt
+++ app/src/main/kotlin/pl/elpassion/ElSpaceApp.kt
@@ -2,6 +2,7 @@ package pl.elpassion
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import io.fabric.sdk.android.Fabric
 import pl.elpassion.common.ContextProvider
 
@@ -9,7 +10,9 @@ class ElSpaceApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Fabric.with(this, Crashlytics())
+        Fabric.with(this, Crashlytics.Builder().core(createCrashlyticsCore()).build())
         ContextProvider.override = { applicationContext }
     }
+
+    private fun createCrashlyticsCore() = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
 }

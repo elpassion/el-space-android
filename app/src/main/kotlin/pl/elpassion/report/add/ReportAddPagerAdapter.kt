d36ef2cd2ddbf9009ac123518785a34dccbf3a8b
Paweł Gajda <pawegio@gmail.com>
2017-01-02 16:43:08 +0100

    [Refactoring] Convert method bodies to expression

--- app/src/main/kotlin/pl/elpassion/report/add/ReportAddPagerAdapter.kt
+++ app/src/main/kotlin/pl/elpassion/report/add/ReportAddPagerAdapter.kt
@@ -3,14 +3,10 @@ package pl.elpassion.report.add
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentStatePagerAdapter
 
-class ReportAddPagerAdapter(private val items: List<ReportAddDetails.View>, activity: ReportAddActivity) : FragmentStatePagerAdapter(activity.supportFragmentManager) {
+class ReportAddPagerAdapter(private val items: List<ReportAddDetails.View>, activity: ReportAddActivity) :
+        FragmentStatePagerAdapter(activity.supportFragmentManager) {
 
+    override fun getItem(position: Int): Fragment = items[position]
 
-    override fun getItem(position: Int): Fragment {
-        return items[position]
-    }
-
-    override fun getCount(): Int {
-        return items.size
-    }
+    override fun getCount(): Int = items.size
 }

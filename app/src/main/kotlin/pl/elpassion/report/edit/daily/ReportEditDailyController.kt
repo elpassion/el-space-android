9feecedf140119de912b382412e92c0cec359eaf
Paweł Gajda <pawegio@gmail.com>
2017-01-04 11:30:39 +0100

    #17 Call Api with correct data on save report

--- app/src/main/kotlin/pl/elpassion/report/edit/daily/ReportEditDailyController.kt
+++ app/src/main/kotlin/pl/elpassion/report/edit/daily/ReportEditDailyController.kt
@@ -1,12 +1,39 @@
 package pl.elpassion.report.edit.daily
 
+import pl.elpassion.api.applySchedulers
+import pl.elpassion.common.extensions.dayOfMonth
+import pl.elpassion.common.extensions.month
+import pl.elpassion.common.extensions.toCalendarDate
+import pl.elpassion.common.extensions.year
 import pl.elpassion.report.DailyReport
 import pl.elpassion.report.edit.ReportEdit
+import kotlin.properties.Delegates
 
-class ReportEditDailyController(val view: ReportEdit.Daily.View) {
+class ReportEditDailyController(val view: ReportEdit.Daily.View,
+                                val editReportApi: ReportEdit.Daily.Service) {
+
+    private var report: DailyReport by Delegates.notNull()
 
     fun onCreate(report: DailyReport) {
         view.showDate(report.date)
+        this.report = report
+    }
+
+    fun onDateSelect(date: String) {
+        val calendar = date.toCalendarDate()
+        report = report.copy(day = calendar.dayOfMonth, month = calendar.month + 1, year = calendar.year)
+    }
+
+    fun onSaveReport() {
+        editReportApi.edit(report)
+                .applySchedulers()
+                .doOnSubscribe { view.showLoader() }
+                .doOnUnsubscribe { view.hideLoader() }
+                .subscribe({
+                    view.close()
+                }, {
+                    view.showError(it)
+                })
     }
 
 }
--- app/src/test/kotlin/pl/elpassion/report/edit/daily/ReportEditDailyControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/report/edit/daily/ReportEditDailyControllerTest.kt
@@ -1,15 +1,31 @@
 package pl.elpassion.report.edit.daily
 
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import pl.elpassion.commons.RxSchedulersRule
 import pl.elpassion.project.dto.newDailyReport
+import pl.elpassion.report.DailyReportType
 import pl.elpassion.report.edit.ReportEdit
+import rx.Completable
 
 class ReportEditDailyControllerTest {
 
+    private val editReportApi = mock<ReportEdit.Daily.Service>()
     private val view = mock<ReportEdit.Daily.View>()
-    private val controller = ReportEditDailyController(view)
+    private val controller = ReportEditDailyController(view, editReportApi)
+
+    @JvmField @Rule
+    val rxSchedulersRule = RxSchedulersRule()
+
+    @Before
+    fun setUp() {
+        stubEditReportApiToReturnSuccess()
+    }
 
     @Test
     fun shouldShowSelectedDateOnCreate() {
@@ -17,5 +33,20 @@ class ReportEditDailyControllerTest {
         controller.onCreate(report)
 
         verify(view).showDate(report.date)
+    }
+
+    @Test
+    fun shouldCallApiWithCorrectDataOnSaveReport() {
+        val report = newDailyReport(year = 2017, month = 7, day = 2, id = 2, reportType = DailyReportType.SICK_LEAVE)
+        controller.onCreate(report)
+
+        controller.onDateSelect("2017-01-03")
+        controller.onSaveReport()
+
+        verify(editReportApi).edit(report.copy(year = 2017, month = 1, day = 3, id = 2, reportType = DailyReportType.SICK_LEAVE))
+    }
+
+    private fun stubEditReportApiToReturnSuccess() {
+        whenever(editReportApi.edit(any())).thenReturn(Completable.complete())
     }
 }

1639a90aa9784bfa6cd19f46a17beb2855ebdabd
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-12 16:22:54 +0100

    Emit event onOpen

--- pacman/src/main/kotlin/pl/elpassion/space/pacman/WebSocketClient.kt
+++ pacman/src/main/kotlin/pl/elpassion/space/pacman/WebSocketClient.kt
@@ -5,43 +5,37 @@ import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
 import rx.Observable
-import rx.Observable.never
-import rx.subjects.BehaviorSubject
+import rx.subjects.PublishSubject
 
 class WebSocketClient(private val api: Api, private val url: String) {
 
-    fun connect(): Observable<Connection> {
+    val subject = PublishSubject.create<Event>()
+
+    fun connect(): Observable<Event> {
         api.connect("", object : WebSocketListener() {
             override fun onOpen(webSocket: WebSocket?, response: Response?) {
-                super.onOpen(webSocket, response)
+                subject.onNext(Event())
             }
 
             override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
-                super.onFailure(webSocket, t, response)
             }
 
             override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
-                super.onClosing(webSocket, code, reason)
             }
 
             override fun onMessage(webSocket: WebSocket?, text: String?) {
-                super.onMessage(webSocket, text)
             }
 
             override fun onMessage(webSocket: WebSocket?, bytes: ByteString?) {
-                super.onMessage(webSocket, bytes)
             }
 
             override fun onClosed(webSocket: WebSocket?, code: Int, reason: String?) {
-                super.onClosed(webSocket, code, reason)
             }
         })
-        return never()
+        return subject
     }
 
-    class Connection {
-        val messages = BehaviorSubject.create<String>()
-    }
+    class Event
 
     interface Api {
         fun connect(url: String, listener: WebSocketListener)
--- pacman/src/test/kotlin/pl/elpassion/space/pacman/WebSocketClientTest.kt
+++ pacman/src/test/kotlin/pl/elpassion/space/pacman/WebSocketClientTest.kt
@@ -3,18 +3,43 @@ package pl.elpassion.space.pacman
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.WebSocketListener
 import org.junit.Test
 import rx.observers.TestSubscriber
+import kotlin.properties.Delegates
 
 class WebSocketClientTest {
 
-    val subscriber = TestSubscriber<String>()
+    val subscriber = TestSubscriber<WebSocketClient.Event>()
     val api = mock<WebSocketClient.Api>()
-    val client = WebSocketClient(api, "")
 
     @Test
     fun shouldConnectToApi() {
+        val client = WebSocketClient(api, "")
         client.connect().subscribe()
         verify(api).connect(any(), any())
+    }
+
+    @Test
+    fun shouldEmitEventOnOpen() {
+        val stub = ApiStub()
+        val client = WebSocketClient(stub, "")
+        client.connect().subscribe(subscriber)
+        stub.listener.onOpen(mock(), createResponseStub())
+        subscriber.assertValueCount(1)
+    }
+
+    private fun createResponseStub() = Response.Builder().request(Request.Builder().url("ws://192.168.1.19:8080/ws").build()).protocol(Protocol.HTTP_2).code(200).build()
+
+    class ApiStub : WebSocketClient.Api {
+
+        var listener : WebSocketListener by Delegates.notNull<WebSocketListener>()
+
+        override fun connect(url: String, listener: WebSocketListener) {
+            this.listener = listener
+        }
     }
 }

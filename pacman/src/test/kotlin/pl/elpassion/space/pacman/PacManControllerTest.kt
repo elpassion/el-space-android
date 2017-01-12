f7510c5835c4dc6d3a24e0d3504e501f02837c02
Karol Kowalski <ordon1313@gmail.com>
2017-01-12 17:13:27 +0100

    [PacManController] show map loading error when loading map fails

--- pacman/src/test/kotlin/pl/elpassion/space/pacman/PacManControllerTest.kt
+++ pacman/src/test/kotlin/pl/elpassion/space/pacman/PacManControllerTest.kt
@@ -6,23 +6,35 @@ import org.junit.Test
 
 class PacManControllerTest {
 
+    val view = mock<PacMan.View>()
     val mapView = mock<PacMan.MapView>()
 
     @Test
     fun shouldLoadMapOnCreate() {
-        PanManController(mapView).onCreate()
+        PanManController(view, mapView).onCreate()
         verify(mapView).loadMap()
+    }
+
+    @Test
+    fun shouldShowMapLoadingErrorWhenLoadingMapFailed() {
+        PanManController(view, mapView).onCreate()
+        verify(view).showMapLoadingError()
     }
 }
 
-class PanManController(val mapView: PacMan.MapView) {
+class PanManController(val view: PacMan.View, val mapView: PacMan.MapView) {
     fun onCreate() {
         mapView.loadMap()
+        view.showMapLoadingError()
     }
 }
 
 interface PacMan {
     interface MapView {
         fun loadMap()
+    }
+
+    interface View {
+        fun showMapLoadingError()
     }
 }

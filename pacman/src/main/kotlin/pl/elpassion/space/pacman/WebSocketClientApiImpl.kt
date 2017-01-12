464f2c303973d93fff0bd058aa27ea838e39abd2
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-12 18:32:52 +0100

    [sockets] Forward close() to WebSocket

--- pacman/src/main/kotlin/pl/elpassion/space/pacman/WebSocketClientApiImpl.kt
+++ pacman/src/main/kotlin/pl/elpassion/space/pacman/WebSocketClientApiImpl.kt
@@ -2,21 +2,23 @@ package pl.elpassion.space.pacman
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.WebSocket
 import okhttp3.WebSocketListener
+import java.io.Closeable
 
 
-class WebSocketClientApiImpl : WebSocketClient.Api {
+class WebSocketClientApiImpl : WebSocketClient.Api, Closeable {
 
-    val okHttpClient by lazy { OkHttpClient() }
+    private val okHttpClient by lazy { OkHttpClient() }
+    lateinit var webSocket: WebSocket
 
     override fun connect(url: String, listener: WebSocketListener) {
-        okHttpClient.newWebSocket(createRequest(url), listener)
+        webSocket = okHttpClient.newWebSocket(createRequest(url), listener)
     }
 
     override fun close() {
+        webSocket.close(0, null)
     }
 
-    private fun createRequest(url: String): Request? {
-        return Request.Builder().url(url) .build()
-    }
+    private fun createRequest(url: String) = Request.Builder().url(url) .build()
 }

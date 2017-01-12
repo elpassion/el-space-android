fd968f65c35ba1e39619bbe7f8459eb2ddf9fa58
Karol Kowalski <ordon1313@gmail.com>
2017-01-12 10:56:58 +0100

    Add IndoorwayMapView

--- pacman/src/main/kotlin/pl/elpassion/space/pacman/PacMan.kt
+++ pacman/src/main/kotlin/pl/elpassion/space/pacman/PacMan.kt
@@ -2,11 +2,23 @@ package pl.elpassion.space.pacman
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.indoorway.android.map.sdk.view.IndoorwayMapView
+
 
 class PacMan : AppCompatActivity() {
+
+    val indoorwayMapView by lazy { findViewById(R.id.mapView) as IndoorwayMapView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pac_man_activity)
+        indoorwayMapView
+                .setOnMapLoadCompletedListener<IndoorwayMapView> {
+
+                }
+                .setOnMapLoadFailedListener<IndoorwayMapView> {
+
+                }
+                .loadMap("hk64fd7-c6k", "5LEUvY1VATo")
     }
 }

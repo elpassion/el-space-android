b89ae7e3a5d3f9feb24e9037d162fe2da0e50657
Paweł Gajda <pawegio@gmail.com>
2017-01-12 10:27:53 +0100

    Configure kotlin in pacman module

--- pacman/src/main/kotlin/pl/elpassion/space/pacman/PacMan.kt
+++ pacman/src/main/kotlin/pl/elpassion/space/pacman/PacMan.kt
@@ -1,7 +1,7 @@
 package pl.elpassion.space.pacman
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class PacMan : AppCompatActivity() {
 

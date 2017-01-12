4732231fa6ff9754f507c02ea15eba4c0b759dc3
Karol Kowalski <ordon1313@gmail.com>
2017-01-12 10:22:23 +0100

    Add Pac-Man module

---
+++ pacman/src/main/java/pl/elpassion/space/pacman/PacMan.kt
@@ -0,0 +1,12 @@
+package pl.elpassion.space.pacman
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PacMan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pac_man_activity)
+    }
+}

3fe0e72bbff38316bdc91d7c9b0f9201c6c538cd
Dominik <dominik.malantowicz@gmail.com>
2017-01-16 16:20:49 +0100

    Have login button

--- app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
+++ app/src/androidTest/kotlin/pl/elpassion/debate/login/DebateLoginActivityTest.kt
@@ -5,10 +5,7 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withInputType
 import android.text.InputType.TYPE_CLASS_NUMBER
 import android.text.InputType.TYPE_NUMBER_VARIATION_NORMAL
-import com.elpassion.android.commons.espresso.hasText
-import com.elpassion.android.commons.espresso.isDisplayed
-import com.elpassion.android.commons.espresso.onId
-import com.elpassion.android.commons.espresso.onText
+import com.elpassion.android.commons.espresso.*
 import org.junit.Rule
 import org.junit.Test
 import pl.elpassion.R
@@ -36,6 +33,14 @@ class DebateLoginActivityTest {
                 .perform(replaceText("123456"))
                 .hasText("12345")
                 .check(matches(withInputType(TYPE_CLASS_NUMBER or TYPE_NUMBER_VARIATION_NORMAL)))
+    }
+
+    @Test
+    fun shouldHaveLoginButton() {
+        onId(R.id.loginButton)
+                .hasText(R.string.login_button)
+                .isDisplayed()
+                .isEnabled()
     }
 }
 

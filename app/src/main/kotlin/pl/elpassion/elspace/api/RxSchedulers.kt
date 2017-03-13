60166d5119cf60ead8b3f5ad6a0388d2b3e1422a
Dominik <dominik.malantowicz@gmail.com>
2017-03-13 13:02:50 +0100

    [refactoring] Remove unused method

--- app/src/main/kotlin/pl/elpassion/elspace/api/RxSchedulers.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/api/RxSchedulers.kt
@@ -1,14 +1,8 @@
 package pl.elpassion.elspace.api
 
 import rx.Completable
-import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
-
-fun <T> Observable<T>.applySchedulers(): Observable<T> =
-        this.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-
 
 fun Completable.applySchedulers(): Completable =
         this.subscribeOn(Schedulers.io())

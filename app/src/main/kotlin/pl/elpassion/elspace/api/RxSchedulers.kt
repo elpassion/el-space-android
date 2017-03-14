4028c1d9d05ba83750bd0e75c8205618ee4f325a
Karol Kowalski <ordon1313@gmail.com>
2017-03-14 13:29:16 +0100

    Add @Deprecated on applySchedulers

--- app/src/main/kotlin/pl/elpassion/elspace/api/RxSchedulers.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/api/RxSchedulers.kt
@@ -4,6 +4,9 @@ import rx.Completable
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
+@Deprecated(
+        message = "Schedulers schould be passed as a clear dependency",
+        replaceWith = ReplaceWith("subscribeOn(schedulers.subscribeOn).observeOn(schedulers.observeOn)"))
 fun Completable.applySchedulers(): Completable =
         this.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())

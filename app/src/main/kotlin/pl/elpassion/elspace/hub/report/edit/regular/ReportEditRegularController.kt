721d887d1ca51982875abdc965a79dc4674f2752
Karol Kowalski <ordon1313@gmail.com>
2017-03-14 18:43:39 +0100

    [#50] Fix api service naming in ReportEditRegularController

--- app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/regular/ReportEditRegularController.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/regular/ReportEditRegularController.kt
@@ -1,7 +1,10 @@
 package pl.elpassion.elspace.hub.report.edit.regular
 
 import pl.elpassion.elspace.api.applySchedulers
-import pl.elpassion.elspace.common.extensions.*
+import pl.elpassion.elspace.common.extensions.dayOfMonth
+import pl.elpassion.elspace.common.extensions.month
+import pl.elpassion.elspace.common.extensions.toCalendarDate
+import pl.elpassion.elspace.common.extensions.year
 import pl.elpassion.elspace.hub.project.Project
 import pl.elpassion.elspace.hub.report.RegularHourlyReport
 import pl.elpassion.elspace.hub.report.edit.ReportEdit
@@ -9,7 +12,7 @@ import rx.Subscription
 import kotlin.properties.Delegates
 
 class ReportEditRegularController(private val view: ReportEdit.Regular.View,
-                                  private val editReportApi: ReportEdit.Regular.Service,
+                                  private val editReportService: ReportEdit.Regular.Service,
                                   private val removeReportApi: ReportEdit.RemoveApi) {
 
     private var report: RegularHourlyReport by Delegates.notNull()
@@ -35,7 +38,7 @@ class ReportEditRegularController(private val view: ReportEdit.Regular.View,
     }
 
     private fun sendEditedReport(description: String, hours: String) {
-        subscription = editReportApi.edit(report.copy(description = description, reportedHours = hours.toDouble()))
+        subscription = editReportService.edit(report.copy(description = description, reportedHours = hours.toDouble()))
                 .applySchedulers()
                 .doOnSubscribe { view.showLoader() }
                 .doOnUnsubscribe { view.hideLoader() }

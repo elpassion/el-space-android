0c4ea85a3317c2751bfd2ecb1116adab2ef0b72f
Paweł Gajda <pawegio@gmail.com>
2017-03-15 11:45:08 +0100

    #30 Show regular form after report type changed to regular report

--- app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditController.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditController.kt
@@ -1,11 +1,19 @@
 package pl.elpassion.elspace.hub.report.edit
 
+import pl.elpassion.elspace.common.extensions.addTo
 import pl.elpassion.elspace.hub.report.Report
+import rx.subscriptions.CompositeSubscription
 
 class ReportEditController(private val report: Report,
                            private val view: ReportEdit.View) {
 
+    private val subscriptions = CompositeSubscription()
+
     fun onCreate() {
         view.showDate(report.date)
+        view.reportTypeChanges()
+                .doOnNext { view.showRegularForm() }
+                .subscribe()
+                .addTo(subscriptions)
     }
 }
--- app/src/test/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditControllerTest.kt
@@ -2,17 +2,35 @@ package pl.elpassion.elspace.hub.report.edit
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Before
 import org.junit.Test
+import pl.elpassion.elspace.hub.project.dto.newDailyReport
 import pl.elpassion.elspace.hub.project.dto.newRegularHourlyReport
+import pl.elpassion.elspace.hub.report.add.ReportType
+import rx.subjects.PublishSubject
 
 class ReportEditControllerTest {
 
     private val view = mock<ReportEdit.View>()
+    private val reportTypeChanges = PublishSubject.create<ReportType>()
+
+    @Before
+    fun setUp() {
+        whenever(view.reportTypeChanges()).thenReturn(reportTypeChanges)
+    }
 
     @Test
     fun shouldShowReportDateOnCreate() {
         val report = newRegularHourlyReport(year = 2017, month = 1, day = 1)
         ReportEditController(report, view).onCreate()
         verify(view).showDate("2017-01-01")
+    }
+
+    @Test
+    fun shouldShowRegularFormAfterReportTypeChangedToRegularReport() {
+        ReportEditController(newDailyReport(), view).onCreate()
+        reportTypeChanges.onNext(ReportType.REGULAR)
+        verify(view).showRegularForm()
     }
 }

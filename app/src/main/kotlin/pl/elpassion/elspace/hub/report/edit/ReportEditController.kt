b31b41652272b333d3b780696cb0cda2881b24cf
Paweł Gajda <pawegio@gmail.com>
2017-03-15 11:01:39 +0100

    #30 Pass interface instead of concrete class to controller

--- app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditController.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/hub/report/edit/ReportEditController.kt
@@ -1,8 +1,8 @@
 package pl.elpassion.elspace.hub.report.edit
 
-import pl.elpassion.elspace.hub.report.RegularHourlyReport
+import pl.elpassion.elspace.hub.report.Report
 
-class ReportEditController(private val report: RegularHourlyReport,
+class ReportEditController(private val report: Report,
                            private val view: ReportEdit.View) {
 
     fun onCreate() {

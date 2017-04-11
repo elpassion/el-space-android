920860d97525f6c86a2939e85953f2f29da0b253
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-04-11 15:24:37 +0200

    #60 Show and hide loader

--- app/src/main/kotlin/pl/elpassion/elspace/hub/login/LoginActivity.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/hub/login/LoginActivity.kt
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_activity.*
 import pl.elpassion.R
 import pl.elpassion.elspace.common.SchedulersSupplier
+import pl.elpassion.elspace.common.hideLoader
+import pl.elpassion.elspace.common.showLoader
 import pl.elpassion.elspace.hub.login.shortcut.ShortcutServiceImpl
 import pl.elpassion.elspace.hub.report.list.ReportListActivity
 import rx.android.schedulers.AndroidSchedulers
@@ -50,9 +52,9 @@ class LoginActivity : AppCompatActivity(), Login.View {
 
     override fun showGoogleTokenError() = Unit
 
-    override fun showLoader() = Unit
+    override fun showLoader() = showLoader(loginCoordinator)
 
-    override fun hideLoader() = Unit
+    override fun hideLoader() = hideLoader(loginCoordinator)
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         googleSingInController.onActivityResult(requestCode, resultCode, data)

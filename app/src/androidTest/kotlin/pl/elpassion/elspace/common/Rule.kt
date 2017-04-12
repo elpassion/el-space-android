6d1626407b8e233d60fcfaad3e39781a8b841bef
Dominik <dominik.malantowicz@gmail.com>
2017-04-12 21:06:15 +0200

    [fix tests] Prevent tests from internet use

--- app/src/androidTest/kotlin/pl/elpassion/elspace/common/Rule.kt
+++ app/src/androidTest/kotlin/pl/elpassion/elspace/common/Rule.kt
@@ -5,6 +5,7 @@ import android.support.test.rule.ActivityTestRule
 import com.nhaarman.mockito_kotlin.mock
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import pl.elpassion.elspace.api.RetrofitProvider
 import pl.elpassion.elspace.hub.project.CachedProjectRepositoryProvider
 import pl.elpassion.elspace.hub.report.add.ReportAdd
 import pl.elpassion.elspace.hub.report.edit.ReportEdit
@@ -14,6 +15,7 @@ import rx.Observable
 inline fun <reified T : Activity> rule(autoStart: Boolean = true, noinline beforeActivity: () -> Unit = { Unit }): ActivityTestRule<T> {
     return object : ActivityTestRule<T>(T::class.java, false, autoStart) {
         override fun apply(base: Statement?, description: Description?): Statement {
+            RetrofitProvider.override = { throw RuntimeException("Internet in tests not allowed!") }
             Animations.disable()
             stubReportAddApi()
             stubProjectRepository()

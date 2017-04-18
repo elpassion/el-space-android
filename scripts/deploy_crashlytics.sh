7f89bc46e6b3421c68bd598b4d25a72ba344acc3
Dominik <dominik.malantowicz@gmail.com>
2017-04-18 17:35:52 +0200

    CircleCI final setup + remove travis.yml

--- scripts/deploy_crashlytics.sh
+++ scripts/deploy_crashlytics.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
 DIR=$(dirname $0)
 echo $DIR
-RELEASE_NOTES=$(git log --format="%cn @ "$TRAVIS_BRANCH"
+COMMIT_MESSAGE=`-1 --pretty=%B`
+RELEASE_NOTES=$(git log --format="%cn @ "$CIRCLE_BRANCH"
 ------
-%B" -n 1 $TRAVIS_COMMIT)
+%B" -n 1 $COMMIT_MESSAGE)
 echo "$RELEASE_NOTES" > $DIR/../crashlytics_release_notes.txt
 ./gradlew :app:crashlyticsUploadDistributionDebug
-fi

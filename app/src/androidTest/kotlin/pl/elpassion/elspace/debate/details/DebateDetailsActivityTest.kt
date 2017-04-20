643a496e8330b788d63696c3ea466db6408f6d9a
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-04-20 17:44:04 +0200

    Show question view

---
+++ app/src/androidTest/kotlin/pl/elpassion/elspace/debate/details/DebateDetailsActivityTest.kt
@@ -0,0 +1,20 @@
+package pl.elpassion.elspace.debate.details
+
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onId
+import org.junit.Rule
+import org.junit.Test
+import pl.elpassion.R
+import pl.elpassion.elspace.common.rule
+
+class DebateDetailsActivityTest {
+
+    @JvmField @Rule
+    val rule = rule<DebateDetailsActivity> {}
+
+    @Test
+    fun shouldShowQuestionView() {
+        onId(R.id.debateDetailsQuestion).isDisplayed()
+    }
+}
+
---
+++ app/src/main/kotlin/pl/elpassion/elspace/debate/details/DebateDetailsActivity.kt
@@ -0,0 +1,13 @@
+package pl.elpassion.elspace.debate.details
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.elpassion.R
+
+class DebateDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.debate_details_activity)
+    }
+}

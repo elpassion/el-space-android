b95046746b6fde69779b195d0cc818217bf41dfa
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-04-24 17:21:16 +0200

    Show debate button

--- app/src/androidTest/kotlin/pl/elpassion/elspace/launch/LauncherActivityTest.kt
+++ app/src/androidTest/kotlin/pl/elpassion/elspace/launch/LauncherActivityTest.kt
@@ -1,17 +1,20 @@
 package pl.elpassion.elspace.launch
 
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onText
 import org.junit.Rule
 import org.junit.Test
+import pl.elpassion.R
 import pl.elpassion.elspace.common.rule
 import pl.elpassion.elspace.launcher.LauncherActivity
 
 class LauncherActivityTest {
 
     @JvmField @Rule
-    val rule = rule<LauncherActivity>{}
+    val rule = rule<LauncherActivity> {}
 
     @Test
-    fun initTest() {
-
+    fun shouldShowDebateButton() {
+        onText(R.string.open_debate).isDisplayed()
     }
 }
--- app/src/main/kotlin/pl/elpassion/elspace/launcher/LauncherActivity.kt
+++ app/src/main/kotlin/pl/elpassion/elspace/launcher/LauncherActivity.kt
@@ -1,5 +1,13 @@
 package pl.elpassion.elspace.launcher
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pl.elpassion.R
 
-class LauncherActivity : AppCompatActivity()
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launcher_activity)
+    }
+}

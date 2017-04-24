d1c228ce89a6bfb29c73eb6cd9668d4bd541ce07
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-04-24 17:08:03 +0200

    Init launcher

---
+++ app/src/androidTest/kotlin/pl/elpassion/elspace/launch/LauncherActivityTest.kt
@@ -0,0 +1,17 @@
+package pl.elpassion.elspace.launch
+
+import org.junit.Rule
+import org.junit.Test
+import pl.elpassion.elspace.common.rule
+import pl.elpassion.elspace.launcher.LauncherActivity
+
+class LauncherActivityTest {
+
+    @JvmField @Rule
+    val rule = rule<LauncherActivity>{}
+
+    @Test
+    fun initTest() {
+
+    }
+}
---
+++ app/src/main/kotlin/pl/elpassion/elspace/launcher/LauncherActivity.kt
@@ -0,0 +1,5 @@
+package pl.elpassion.elspace.launcher
+
+import android.support.v7.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity()

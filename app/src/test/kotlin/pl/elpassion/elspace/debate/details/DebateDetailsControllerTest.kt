a4c6620366a7ca207c0eb2af8265ff3fd9578162
Paweł Gajda <pawegio@gmail.com>
2017-05-19 14:43:46 +0200

    [Refactoring] Extract controller factory method

--- app/src/test/kotlin/pl/elpassion/elspace/debate/details/DebateDetailsControllerTest.kt
+++ app/src/test/kotlin/pl/elpassion/elspace/debate/details/DebateDetailsControllerTest.kt
@@ -1,6 +1,7 @@
 package pl.elpassion.elspace.debate.details
 
 import com.nhaarman.mockito_kotlin.*
+import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.TestScheduler
 import io.reactivex.subjects.CompletableSubject
@@ -15,7 +16,6 @@ class DebateDetailsControllerTest {
 
     private val api = mock<DebateDetails.Api>()
     private val view = mock<DebateDetails.View>()
-    private val controller = DebateDetailsController(api, view, SchedulersSupplier(Schedulers.trampoline(), Schedulers.trampoline()))
     private val debateDetailsSubject = SingleSubject.create<DebateData>()
     private val sendVoteSubject = CompletableSubject.create()
 
@@ -27,19 +27,19 @@ class DebateDetailsControllerTest {
 
     @Test
     fun shouldCallApiWithGivenTokenOnCreate() {
-        controller.onCreate(token = "token")
+        createController().onCreate(token = "token")
         verify(api).getDebateDetails(token = "token")
     }
 
     @Test
     fun shouldReallyCallApiWithGivenTokenOnCreate() {
-        controller.onCreate(token = "otherToken")
+        createController().onCreate(token = "otherToken")
         verify(api).getDebateDetails(token = "otherToken")
     }
 
     @Test
     fun shouldShowDebateDetailsReturnedFromApiCallOnView() {
-        controller.onCreate("token")
+        createController().onCreate("token")
         val debateDetails = createDebateData()
         returnFromApi(debateDetails)
         verify(view).showDebateDetails(debateDetails)
@@ -47,28 +47,27 @@ class DebateDetailsControllerTest {
 
     @Test
     fun shouldShowLoaderOnApiCall() {
-        controller.onCreate("token")
+        createController().onCreate("token")
         verify(view).showLoader()
     }
 
     @Test
     fun shouldHideLoaderWhenApiCallFinishes() {
-        controller.onCreate("token")
+        createController().onCreate("token")
         returnFromApi(createDebateData())
         verify(view).hideLoader()
     }
 
     @Test
     fun shouldNotHideLoaderWhenApiCallIsNotFinished() {
-        controller.onCreate("token")
+        createController().onCreate("token")
         verify(view, never()).hideLoader()
     }
 
     @Test
     fun shouldUseGivenSchedulerToSubscribeOn() {
         val subscribeOn = TestScheduler()
-        val controller = DebateDetailsController(api, view, SchedulersSupplier(subscribeOn, Schedulers.trampoline()))
-        controller.onCreate("token")
+        createController(subscribeOn = subscribeOn).onCreate("token")
         returnFromApi(createDebateData())
         verify(view, never()).hideLoader()
         subscribeOn.triggerActions()
@@ -78,8 +77,7 @@ class DebateDetailsControllerTest {
     @Test
     fun shouldUseGivenSchedulerToObserveOn() {
         val observeOn = TestScheduler()
-        val controller = DebateDetailsController(api, view, SchedulersSupplier(Schedulers.trampoline(), observeOn))
-        controller.onCreate("token")
+        createController(observeOn = observeOn).onCreate("token")
         returnFromApi(createDebateData())
         verify(view, never()).hideLoader()
         observeOn.triggerActions()
@@ -88,7 +86,7 @@ class DebateDetailsControllerTest {
 
     @Test
     fun shouldShowErrorWhenApiCallFails() {
-        controller.onCreate("token")
+        createController().onCreate("token")
         val exception = RuntimeException()
         debateDetailsSubject.onError(exception)
         verify(view).showDebateDetailsError(exception)
@@ -96,59 +94,60 @@ class DebateDetailsControllerTest {
 
     @Test
     fun shouldHideLoaderOnDestroyIfApiCallIsRunning() {
-        controller.onCreate("token")
-        controller.onDestroy()
+        createController().run {
+            onCreate("token")
+            onDestroy()
+        }
         verify(view).hideLoader()
     }
 
     @Test
     fun shouldNotHideLoaderOnDestroyIfApiCallWasntTriggered() {
-        controller.onDestroy()
+        createController().onDestroy()
         verify(view, never()).hideLoader()
     }
 
     @Test
     fun shouldCallApiOnDebateDetailsRefresh() {
-        controller.onDebateDetailsRefresh("newToken")
+        createController().onDebateDetailsRefresh("newToken")
         verify(api).getDebateDetails("newToken")
     }
 
     @Test
     fun shouldCallApiWithSelectedAnswerOnVote() {
-        controller.onVote("token", Answer(2, "answerNegative"))
+        createController().onVote("token", Answer(2, "answerNegative"))
         verify(api).vote("token", Answer(2, "answerNegative"))
     }
 
     @Test
     fun shouldReallyCallApiWithSelectedAnswerOnVote() {
-        controller.onVote("differentToken", Answer(1, "answer"))
+        createController().onVote("differentToken", Answer(1, "answer"))
         verify(api).vote("differentToken", Answer(1, "answer"))
     }
 
     @Test
     fun shouldShowLoaderOnVote() {
-        controller.onVote("token", createAnswer())
+        createController().onVote("token", createAnswer())
         verify(view).showLoader()
     }
 
     @Test
     fun shouldHideLoaderWhenVoteApiCallIsFinished() {
-        controller.onVote("token", createAnswer())
+        createController().onVote("token", createAnswer())
         sendVoteSubject.onComplete()
         verify(view).hideLoader()
     }
 
     @Test
     fun shouldNotHideLoaderIfVoteCallIsStillInProgress() {
-        controller.onVote("token", createAnswer())
+        createController().onVote("token", createAnswer())
         verify(view, never()).hideLoader()
     }
 
     @Test
     fun shouldUseGivenSchedulerToSubscribeOnWhenVote() {
         val subscribeOn = TestScheduler()
-        val controller = DebateDetailsController(api, view, SchedulersSupplier(subscribeOn, Schedulers.trampoline()))
-        controller.onVote("token", createAnswer())
+        createController(subscribeOn = subscribeOn).onVote("token", createAnswer())
         sendVoteSubject.onComplete()
         verify(view, never()).hideLoader()
         subscribeOn.triggerActions()
@@ -158,8 +157,7 @@ class DebateDetailsControllerTest {
     @Test
     fun shouldUseGivenSchedulerToObserveOnWhenVote() {
         val observeOn = TestScheduler()
-        val controller = DebateDetailsController(api, view, SchedulersSupplier(Schedulers.trampoline(), observeOn))
-        controller.onVote("token", createAnswer())
+        createController(observeOn = observeOn).onVote("token", createAnswer())
         sendVoteSubject.onComplete()
         verify(view, never()).hideLoader()
         observeOn.triggerActions()
@@ -168,25 +166,31 @@ class DebateDetailsControllerTest {
 
     @Test
     fun shouldHideLoaderOnDestroyIfVoteCallIsStillInProgress() {
-        controller.onVote("", createAnswer())
-        controller.onDestroy()
+        createController().run {
+            onVote("", createAnswer())
+            onDestroy()
+        }
         verify(view).hideLoader()
     }
 
     @Test
     fun shouldShowSuccessWhenVoteSuccessfully() {
-        controller.onVote("", createAnswer())
+        createController().onVote("", createAnswer())
         sendVoteSubject.onComplete()
         verify(view).showVoteSuccess()
     }
 
     @Test
     fun shouldShowErrorWhenVoteFailed() {
-        controller.onVote("", createAnswer())
+        createController().onVote("", createAnswer())
         val exception = RuntimeException()
         sendVoteSubject.onError(exception)
         verify(view).showVoteError(exception)
     }
+
+    private fun createController(subscribeOn: Scheduler = Schedulers.trampoline(),
+                                 observeOn: Scheduler = Schedulers.trampoline()) =
+            DebateDetailsController(api, view, SchedulersSupplier(subscribeOn, observeOn))
 
     private fun returnFromApi(debateData: DebateData) {
         debateDetailsSubject.onSuccess(debateData)

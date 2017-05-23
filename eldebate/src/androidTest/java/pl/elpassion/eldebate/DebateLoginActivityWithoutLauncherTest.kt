e8a9e4af9104ce9cd91ed77828add718eb3443d3
Karol Kowalski <ordon1313@gmail.com>
2017-05-23 13:06:50 +0200

    Add kotlin support for tests in debate

--- eldebate/src/androidTest/java/pl/elpassion/eldebate/DebateLoginActivityWithoutLauncherTest.kt
+++ eldebate/src/androidTest/java/pl/elpassion/eldebate/DebateLoginActivityWithoutLauncherTest.kt
@@ -1,19 +1,23 @@
 package pl.elpassion.eldebate
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.rule.ActivityTestRule
 import com.elpassion.android.commons.espresso.doesNotExist
 import org.junit.Rule
 import org.junit.Test
-import pl.elpassion.elspace.common.onToolbarBackArrow
-import pl.elpassion.elspace.common.rule
+import pl.elpassion.R
 import pl.elpassion.elspace.debate.login.DebateLoginActivity
 
 class DebateLoginActivityWithoutLauncherTest {
 
     @JvmField @Rule
-    val rule = rule<DebateLoginActivity>()
+    val rule = ActivityTestRule<DebateLoginActivity>(DebateLoginActivity::class.java)
 
     @Test
     fun shouldShowToolbarWithoutBackArrow() {
         onToolbarBackArrow().doesNotExist()
     }
+
+    fun onToolbarBackArrow() = Espresso.onView(ViewMatchers.withContentDescription(R.string.abc_action_bar_up_description))
 }

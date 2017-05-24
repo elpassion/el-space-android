d575b587d90378babbbed2bdc22eedf4ba205f01
Paweł Gajda <pawegio@gmail.com>
2017-05-24 11:20:11 +0200

    Rename modules - add not included el-debate-app module

---
+++ el-debate-app/src/androidTest/java/pl/elpassion/eldebate/DebateLoginActivityWithoutLauncherTest.kt
@@ -0,0 +1,25 @@
+package pl.elpassion.eldebate
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.rule.ActivityTestRule
+import com.elpassion.android.commons.espresso.doesNotExist
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import pl.elpassion.R
+import pl.elpassion.elspace.debate.login.DebateLoginActivity
+
+class DebateLoginActivityWithoutLauncherTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<DebateLoginActivity>(DebateLoginActivity::class.java)
+
+    @Test
+    @Ignore
+    fun shouldShowToolbarWithoutBackArrow() {
+        onToolbarBackArrow().doesNotExist()
+    }
+
+    fun onToolbarBackArrow() = Espresso.onView(ViewMatchers.withContentDescription(R.string.abc_action_bar_up_description))
+}

bcaf38cd369b1c80a8e070ba745f5fee12ca21b5
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-05-31 13:18:55 +0200

    #77 Start comment activity when comment button clicked

---
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/comment/CommentActivity.kt
@@ -0,0 +1,19 @@
+package pl.elpassion.elspace.debate.comment
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.elpassion.R
+
+class CommentActivity : AppCompatActivity() {
+
+    companion object {
+        fun start(context: Context) = context.startActivity(Intent(context, CommentActivity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.debate_comment_activity)
+    }
+}

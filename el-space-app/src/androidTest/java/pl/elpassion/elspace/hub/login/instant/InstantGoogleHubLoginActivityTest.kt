1e3d445bb147c91ef90774fc033943b348ad47af
Karol Kowalski <ordon1313@gmail.com>
2017-06-01 14:46:18 +0200

    #89 Add InstantGoogleHubLoginActivity

---
+++ el-space-app/src/androidTest/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivityTest.kt
@@ -0,0 +1,17 @@
+package pl.elpassion.elspace.hub.login.instant
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import pl.elpassion.elspace.common.rule
+
+class InstantGoogleHubLoginActivityTest {
+
+    @Rule @JvmField
+    val rule = rule<InstantGoogleHubLoginActivity>()
+
+    @Test
+    fun shouldStartActivity() {
+
+    }
+}
---
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivity.kt
@@ -0,0 +1,8 @@
+package pl.elpassion.elspace.hub.login.instant
+
+import android.support.v7.app.AppCompatActivity
+
+class InstantGoogleHubLoginActivity : AppCompatActivity() {
+
+
+}

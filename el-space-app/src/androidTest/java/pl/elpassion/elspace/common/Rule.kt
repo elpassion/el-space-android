6d72204e528a93bae638f482845cb5bda66ffc92
Dominik <dominik.malantowicz@gmail.com>
2017-06-01 13:40:04 +0200

    Fix compilation errors

--- el-space-app/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
+++ el-space-app/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
@@ -3,6 +3,7 @@ package pl.elpassion.elspace.common
 import android.app.Activity
 import android.support.test.rule.ActivityTestRule
 import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.Completable
 import io.reactivex.Observable
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
@@ -12,7 +13,6 @@ import pl.elpassion.elspace.hub.login.GoogleSingInDI
 import pl.elpassion.elspace.hub.project.CachedProjectRepositoryProvider
 import pl.elpassion.elspace.hub.report.add.ReportAdd
 import pl.elpassion.elspace.hub.report.edit.ReportEdit
-import pl.elpassion.elspace.hub.report.list.service.ReportFromApi
 
 inline fun <reified T : Activity> rule(autoStart: Boolean = true, noinline beforeActivity: () -> Unit = { Unit }): ActivityTestRule<T> {
     return object : ActivityTestRule<T>(T::class.java, false, autoStart) {
@@ -37,9 +37,9 @@ inline fun <reified T : Activity> rule(autoStart: Boolean = true, noinline befor
 fun stubReportEditApi() {
     ReportEdit.ApiProvider.override = {
         object : ReportEdit.Api {
-            override fun removeReport(reportId: Long) = Observable.just(emptyList<ReportFromApi>())
+            override fun removeReport(reportId: Long) = Completable.complete()
 
-            override fun editReport(id: Long, reportType: Int, date: String, reportedHour: String?, description: String?, projectId: Long?) = Observable.just(Unit)
+            override fun editReport(id: Long, reportType: Int, date: String, reportedHour: String?, description: String?, projectId: Long?) = Completable.complete()
         }
     }
 }

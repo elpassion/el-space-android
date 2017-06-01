2ff67a085390735a34d018952c614f31f1c89710
Karol Kowalski <ordon1313@gmail.com>
2017-06-01 15:43:51 +0200

    #89 Start Google login intent on create

--- el-space-app/src/androidTest/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivityTest.kt
+++ el-space-app/src/androidTest/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivityTest.kt
@@ -1,6 +1,8 @@
 package pl.elpassion.elspace.hub.login.instant
 
+import android.app.Activity
 import android.content.Context
+import com.google.android.gms.common.api.GoogleApiClient
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
@@ -14,11 +16,13 @@ class InstantGoogleHubLoginActivityTest {
 
     val repository = mock<InstantGoogleHubLogin.Repository>()
     val openOnLoggedInScreen = mock<(Context) -> Unit>()
+    val startGoogleSignInActivity = mock<(Activity, () -> GoogleApiClient, Int) -> Unit>()
 
     @Rule @JvmField
     val rule = rule<InstantGoogleHubLoginActivity>(autoStart = false) {
         InstantGoogleHubLoginActivity.provideRepository = { repository }
         InstantGoogleHubLoginActivity.openOnLoggedInScreen = openOnLoggedInScreen
+        InstantGoogleHubLoginActivity.startGoogleSignInActivity = startGoogleSignInActivity
     }
 
     @Test
@@ -26,5 +30,12 @@ class InstantGoogleHubLoginActivityTest {
         whenever(repository.readToken()).thenReturn("token")
         rule.startActivity()
         verify(openOnLoggedInScreen).invoke(any())
+    }
+
+    @Test
+    fun shouldStartGoogleLoginIntentOnCreate() {
+        whenever(repository.readToken()).thenReturn(null)
+        rule.startActivity()
+        verify(startGoogleSignInActivity).invoke(any(), any(), any())
     }
 }
--- el-space-app/src/main/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivity.kt
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivity.kt
@@ -1,18 +1,37 @@
 package pl.elpassion.elspace.hub.login.instant
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
+import pl.elpassion.elspace.R
 
 class InstantGoogleHubLoginActivity : AppCompatActivity() {
+
+    private val googleApiClientObject: GoogleApiClient by lazy {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build()
+        GoogleApiClient.Builder(this)
+                .enableAutoManage(this, {})
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         openOnLoggedInScreen(this)
+        startGoogleSignInActivity(this, { googleApiClientObject }, RC_SIGN_IN)
     }
 
     companion object {
+        private val RC_SIGN_IN = 64927
         lateinit var provideRepository: () -> InstantGoogleHubLogin.Repository
         lateinit var openOnLoggedInScreen: (Context) -> Unit
+        lateinit var startGoogleSignInActivity: (Activity, () -> GoogleApiClient, Int) -> Unit
     }
 }

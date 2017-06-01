431ec54bc0d25a4467aa8acaed941b4038fd54de
Karol Kowalski <ordon1313@gmail.com>
2017-06-01 15:23:07 +0200

    #89 Open on logged in screen if user is logged in on create

--- el-space-app/src/androidTest/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivityTest.kt
+++ el-space-app/src/androidTest/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivityTest.kt
@@ -1,17 +1,30 @@
 package pl.elpassion.elspace.hub.login.instant
 
-import org.junit.Assert.*
+import android.content.Context
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Rule
 import org.junit.Test
 import pl.elpassion.elspace.common.rule
+import pl.elpassion.elspace.common.startActivity
 
 class InstantGoogleHubLoginActivityTest {
 
+    val repository = mock<InstantGoogleHubLogin.Repository>()
+    val openOnLoggedInScreen = mock<(Context) -> Unit>()
+
     @Rule @JvmField
-    val rule = rule<InstantGoogleHubLoginActivity>()
+    val rule = rule<InstantGoogleHubLoginActivity>(autoStart = false) {
+        InstantGoogleHubLoginActivity.provideRepository = { repository }
+        InstantGoogleHubLoginActivity.openOnLoggedInScreen = openOnLoggedInScreen
+    }
 
     @Test
-    fun shouldStartActivity() {
-
+    fun shouldOpenOnLoggedInScreenIfUserIsLoggedInOnCreate() {
+        whenever(repository.readToken()).thenReturn("token")
+        rule.startActivity()
+        verify(openOnLoggedInScreen).invoke(any())
     }
 }
--- el-space-app/src/main/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivity.kt
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/login/instant/InstantGoogleHubLoginActivity.kt
@@ -1,8 +1,18 @@
 package pl.elpassion.elspace.hub.login.instant
 
+import android.content.Context
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class InstantGoogleHubLoginActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        openOnLoggedInScreen(this)
+    }
 
+    companion object {
+        lateinit var provideRepository: () -> InstantGoogleHubLogin.Repository
+        lateinit var openOnLoggedInScreen: (Context) -> Unit
+    }
 }

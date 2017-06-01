2cfcbf19ab4a2048c276de45719c07c9ef1f5532
Karol Kowalski <ordon1313@gmail.com>
2017-06-01 21:45:53 +0200

    Move decrypting to script file

---
+++ scripts/decrypt-el-space-release.sh
@@ -0,0 +1,5 @@
+openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-server-client-id.xml.enc -out el-space-app/src/release/res/values/google-server-client-id.xml
+openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-services-release.json.enc -out el-space-app/google-services.json
+openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-signing-credentials.gradle.enc -out el-space-app/src/release/google-signing-credentials.gradle
+openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-play-api-key.json.enc -out el-space-app/src/release/google-play-api-key.json
+openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/release-keystore.jks.enc -out el-space-app/src/release/release-keystore.jks

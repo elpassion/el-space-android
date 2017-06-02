79b9a3c4b39ce6956d67971c8f1c76361cca3374
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-06-02 08:58:17 +0200

    #77 Use given scheduler to subscribe on when send comment

--- el-debate/src/main/java/pl/elpassion/elspace/debate/comment/DebateCommentController.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/comment/DebateCommentController.kt
@@ -1,11 +1,15 @@
 package pl.elpassion.elspace.debate.comment
 
+import pl.elpassion.elspace.common.SchedulersSupplier
+
 class DebateCommentController(
         private val view: DebateComment.View,
-        private val api: DebateComment.Api) {
+        private val api: DebateComment.Api,
+        private val schedulers: SchedulersSupplier) {
 
     fun sendComment(message: String) {
         api.comment(message)
+                .subscribeOn(schedulers.backgroundScheduler)
                 .doOnSubscribe { view.showLoader() }
                 .doFinally(view::hideLoader)
                 .subscribe()

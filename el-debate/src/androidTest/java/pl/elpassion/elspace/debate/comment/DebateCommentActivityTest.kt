be9678ad3541db5c0969822c926363573f9d6953
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-06-02 13:24:25 +0200

    #77 Have correct comment input

--- el-debate/src/androidTest/java/pl/elpassion/elspace/debate/comment/DebateCommentActivityTest.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/debate/comment/DebateCommentActivityTest.kt
@@ -1,7 +1,9 @@
 package pl.elpassion.elspace.debate.comment
 
-import com.elpassion.android.commons.espresso.isDisplayed
-import com.elpassion.android.commons.espresso.onText
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.text.InputType
+import com.elpassion.android.commons.espresso.*
 import org.junit.Rule
 import org.junit.Test
 import pl.elpassion.R
@@ -15,5 +17,14 @@ class DebateCommentActivityTest {
     @Test
     fun shouldShowTitle() {
         onText(R.string.debate_comment_title).isDisplayed()
+    }
+
+    @Test
+    fun shouldHaveCorrectCommentInput() {
+        onId(R.id.debateCommentInputText)
+                .isDisplayed()
+                .replaceText("mess")
+                .hasText("mess")
+                .check(ViewAssertions.matches(ViewMatchers.withInputType(InputType.TYPE_CLASS_TEXT)))
     }
 }

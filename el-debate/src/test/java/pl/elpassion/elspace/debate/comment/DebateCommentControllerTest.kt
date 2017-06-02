63977979ebd34f4c4ad22e05d7270b1a80275abf
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-06-02 08:58:17 +0200

    #77 Use given scheduler to subscribe on when send comment

--- el-debate/src/test/java/pl/elpassion/elspace/debate/comment/DebateCommentControllerTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/comment/DebateCommentControllerTest.kt
@@ -1,16 +1,19 @@
 package pl.elpassion.elspace.debate.comment
 
 import com.nhaarman.mockito_kotlin.*
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.TestScheduler
 import io.reactivex.subjects.CompletableSubject
 import org.junit.Before
 import org.junit.Test
+import pl.elpassion.elspace.common.SchedulersSupplier
 
 class DebateCommentControllerTest {
 
     private val view = mock<DebateComment.View>()
     private val api = mock<DebateComment.Api>()
     private val commentSubject = CompletableSubject.create()
-    private val controller = DebateCommentController(view, api)
+    private val controller = DebateCommentController(view, api, SchedulersSupplier(Schedulers.trampoline(), Schedulers.trampoline()))
 
     @Before
     fun setUp() {
@@ -40,5 +43,16 @@ class DebateCommentControllerTest {
     fun shouldNotHideLoaderIfSendCommentCallIsStillInProgress() {
         controller.sendComment("message")
         verify(view, never()).hideLoader()
+    }
+
+    @Test
+    fun shouldUseGivenSchedulerToSubscribeOnWhenSendComment() {
+        val subscribeOn = TestScheduler()
+        val controller = DebateCommentController(view, api, SchedulersSupplier(subscribeOn, Schedulers.trampoline()))
+        controller.sendComment("message")
+        commentSubject.onComplete()
+        verify(view, never()).hideLoader()
+        subscribeOn.triggerActions()
+        verify(view).hideLoader()
     }
 }

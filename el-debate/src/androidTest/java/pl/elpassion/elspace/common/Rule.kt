029996df4de93d334600fd38cf7872fe64023c46
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-06-05 15:17:27 +0200

    [fix] Stub api provider

--- el-debate/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.support.test.rule.ActivityTestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import pl.elpassion.elspace.api.DebateRetrofitProvider
 
 inline fun <reified T : Activity> rule(autoStart: Boolean = true, noinline beforeActivity: () -> Unit = { Unit }): ActivityTestRule<T> {
     return object : ActivityTestRule<T>(T::class.java, false, autoStart) {
         override fun apply(base: Statement?, description: Description?): Statement {
+            DebateRetrofitProvider.override = { throw RuntimeException("Internet in tests not allowed!") }
             Animations.areEnabled = false
             return super.apply(base, description)
         }

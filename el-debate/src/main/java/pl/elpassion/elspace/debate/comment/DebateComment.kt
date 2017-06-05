78a5553423137a66eecc088a771bebf6e91b66ab
maciej.przybyl <maciej.przybyl@elpassion.com>
2017-06-05 17:26:23 +0200

    #77 Send comment to real api

--- el-debate/src/main/java/pl/elpassion/elspace/debate/comment/DebateComment.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/comment/DebateComment.kt
@@ -3,11 +3,17 @@ package pl.elpassion.elspace.debate.comment
 import io.reactivex.Completable
 import pl.elpassion.elspace.api.DebateRetrofitProvider
 import pl.elpassion.elspace.common.Provider
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface DebateComment {
 
     interface Api {
-        fun comment(token: String, message: String): Completable
+        @FormUrlEncoded
+        @POST("comment")
+        fun comment(@Header("Authorization") token: String, @Field("text") message: String): Completable
     }
 
     interface View {

d1f768e03fe2fa051fa3e9a52dff23d7b48655de
Paweł Gajda <pawegio@gmail.com>
2017-06-06 15:26:41 +0200

    Allow use of inner memory in tests

--- el-debate/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/common/Rule.kt
@@ -5,12 +5,10 @@ import android.support.test.rule.ActivityTestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 import pl.elpassion.elspace.api.DebateRetrofitProvider
-import pl.elpassion.elspace.debate.DebateTokenRepositoryProvider
 
 inline fun <reified T : Activity> rule(autoStart: Boolean = true, noinline beforeActivity: () -> Unit = { Unit }): ActivityTestRule<T> {
     return object : ActivityTestRule<T>(T::class.java, false, autoStart) {
         override fun apply(base: Statement?, description: Description?): Statement {
-            DebateTokenRepositoryProvider.override = { throw RuntimeException("Inner memory in tests not allowed!") }
             DebateRetrofitProvider.override = { throw RuntimeException("Internet in tests not allowed!") }
             Animations.areEnabled = false
             return super.apply(base, description)

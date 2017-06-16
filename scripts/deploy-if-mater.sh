16c175363abd453709659e1559e92d46c3851911
Karol Kowalski <ordon1313@gmail.com>
2017-06-16 15:04:26 +0200

    #104 Configure release from TeamCity

--- scripts/deploy-if-mater.sh
+++ scripts/deploy-if-mater.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [ "master" = `git rev-parse --abbrev-ref HEAD` ]; then
     ./scripts/decrypt-el-space-release.sh
+    ./scripts/decrypt-el-debate-release.sh
     ./gradlew :el-space-app:publishApkRelease
+    ./gradlew :el-debate-app:publishApkRelease
 fi

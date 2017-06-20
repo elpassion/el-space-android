4b3ef622024e2e25d4873bc4d1a6a7d74417a017
macPry <maciej.przybyl@elpassion.pl>
2017-06-20 13:49:48 +0200

    #106 Highlight neutral answer when last answer was neutral

--- el-debate/src/androidTest/java/pl/elpassion/elspace/debate/details/DebateDetailsActivityTest.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/debate/details/DebateDetailsActivityTest.kt
@@ -134,6 +134,15 @@ class DebateDetailsActivityTest {
     }
 
     @Test
+    fun shouldHighlightNeutralAnswerWhenLastAnswerWasNeutral() {
+        startActivity()
+        debateDetailsSubject.onSuccess(createDebateData(lastAnswerId = 3))
+        onId(R.id.debatePositiveAnswerImage).hasImage(R.drawable.hand_positive_inactive)
+        onId(R.id.debateNegativeAnswerImage).hasImage(R.drawable.hand_negative_inactive)
+        onId(R.id.debateNeutralAnswerImage).hasImage(R.drawable.hand_neutral_active)
+    }
+
+    @Test
     fun shouldShowLoaderWhenCallingApi() {
         startActivity()
         onId(R.id.loader).isDisplayed()

b7cf430f5c870489926f762323a3f981fe210a46
Dominik <dominik.malantowicz@gmail.com>
2017-07-17 12:30:37 +0200

    #131 [fix] Fix random test with InstrumentationRegistry.getInstrumentation().waitForIdleSync() instead of sleep

--- el-debate/src/androidTest/java/pl/elpassion/elspace/debate/details/DebateDetailsActivityTest.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/debate/details/DebateDetailsActivityTest.kt
@@ -21,7 +21,6 @@ import pl.elpassion.elspace.common.stubAllIntents
 import pl.elpassion.elspace.dabate.details.createDebateData
 import pl.elpassion.elspace.dabate.details.createHttpException
 import pl.elpassion.elspace.debate.comment.DebateCommentActivity
-import java.lang.Thread.sleep
 
 class DebateDetailsActivityTest {
 
@@ -184,7 +183,7 @@ class DebateDetailsActivityTest {
     fun shouldCallApiSecondTimeOnRefreshClickedWhenPreviousCallFailed() {
         startActivity()
         debateDetailsSubject.onError(RuntimeException())
-        sleep(100)
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
         onText(R.string.debate_details_error_refresh).click()
         verify(apiMock, times(2)).getDebateDetails(any())
     }

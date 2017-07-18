4a0c5c401b29229f9afd2ce54371d742c4d74ebf
Dominik <dominik.malantowicz@gmail.com>
2017-07-18 12:08:21 +0200

    #122 Add crashlytics to el debate

--- el-debate-app/src/main/java/pl/elpassion/eldebate/ElDebateApp.kt
+++ el-debate-app/src/main/java/pl/elpassion/eldebate/ElDebateApp.kt
@@ -1,6 +1,8 @@
 package pl.elpassion.eldebate
 
 import android.support.multidex.MultiDexApplication
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import pl.elpassion.elspace.common.ContextProvider
 import pl.elpassion.elspace.debate.login.DebateLoginActivity
 
@@ -9,7 +11,7 @@ class ElDebateApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         ContextProvider.override = { applicationContext }
-
+        Fabric.with(this, Crashlytics())
         DebateLoginActivity.showBackArrow = false
     }
 }

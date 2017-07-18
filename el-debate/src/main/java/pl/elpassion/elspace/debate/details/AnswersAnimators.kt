fb519a23bccfdf7ceb3a0212c2dfe51c6b9054ee
Dominik <dominik.malantowicz@gmail.com>
2017-07-18 12:16:42 +0200

    [refactoring] Use goodies from kotlin to make code simpler and nicer

--- el-debate/src/main/java/pl/elpassion/elspace/debate/details/AnswersAnimators.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/details/AnswersAnimators.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.debate_details_activity.view.*
 import pl.elpassion.R
 import pl.elpassion.elspace.common.Animations
@@ -25,49 +26,27 @@ class AnswersAnimators(private val view: View, private val context: Context) {
 
     private val neutralAnswerAnimator by lazy { view.debateNeutralAnswerImage.drawable as Animatable }
 
-    private fun setPositiveActive() {
-        view.debatePositiveAnswerImage.apply {
-            tag = BLUE_DEBATE_POSITIVE
-            setDrawableTintColor(drawable, BLUE_DEBATE_POSITIVE)
-        }
-        view.debateNegativeAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
-        view.debateNeutralAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
+    private fun setPositiveActive() = view.run {
+        debatePositiveAnswerImage.setTintColor(BLUE_DEBATE_POSITIVE)
+        debateNegativeAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
+        debateNeutralAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
     }
 
-    private fun setNegativeActive() {
-        view.debatePositiveAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
-        view.debateNegativeAnswerImage.apply {
-            tag = RED_DEBATE_NEGATIVE
-            setDrawableTintColor(drawable, RED_DEBATE_NEGATIVE)
-        }
-        view.debateNeutralAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
+    private fun setNegativeActive() = view.run {
+        debatePositiveAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
+        debateNegativeAnswerImage.setTintColor(RED_DEBATE_NEGATIVE)
+        debateNeutralAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
     }
 
-    private fun setNeutralActive() {
-        view.debatePositiveAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
-        view.debateNegativeAnswerImage.apply {
-            tag = GREY_DEBATE_INACTIVE
-            setDrawableTintColor(drawable, GREY_DEBATE_INACTIVE)
-        }
-        view.debateNeutralAnswerImage.apply {
-            tag = GREY_DEBATE_NEUTRAL
-            setDrawableTintColor(drawable, GREY_DEBATE_NEUTRAL)
-        }
+    private fun setNeutralActive() = view.run {
+        debatePositiveAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
+        debateNegativeAnswerImage.setTintColor(GREY_DEBATE_INACTIVE)
+        debateNeutralAnswerImage.setTintColor(GREY_DEBATE_NEUTRAL)
+    }
+
+    private fun ImageView.setTintColor(tintColor: Int) {
+        tag = tintColor
+        setDrawableTintColor(drawable, tintColor)
     }
 
     private fun setDrawableTintColor(drawable: Drawable, color: Int) = DrawableCompat.setTint(drawable, ContextCompat.getColor(context, color))

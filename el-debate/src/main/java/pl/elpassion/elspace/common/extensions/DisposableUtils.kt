a1fd7a469a944c11f57d791e9e7802e09663de44
macPry <maciej.przybyl@elpassion.pl>
2017-07-19 18:22:08 +0200

    #143 Remove unused file

--- el-debate/src/main/java/pl/elpassion/elspace/common/extensions/DisposableUtils.kt
+++
@@ -1,6 +0,0 @@
-package pl.elpassion.elspace.common.extensions
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-fun Disposable.addTo(subscription: CompositeDisposable) = subscription.add(this)

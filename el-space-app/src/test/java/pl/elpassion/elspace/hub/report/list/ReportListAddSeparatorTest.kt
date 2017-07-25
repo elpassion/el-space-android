cd21945cbe7c9219288cd2109f84b8aa8814fe62
macPry <maciej.przybyl@elpassion.pl>
2017-07-25 15:29:00 +0200

    #145 Have separator between not filled in day and filled in

--- el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListAddSeparatorTest.kt
+++ el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListAddSeparatorTest.kt
@@ -1,6 +1,7 @@
 package pl.elpassion.elspace.hub.report.list
 
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import pl.elpassion.elspace.hub.project.dto.newRegularHourlyReport
 
@@ -12,4 +13,14 @@ class ReportListAddSeparatorTest {
         val givenAdapters = addSeparators(listOf(newRegularHourlyReport(), newRegularHourlyReport()))
         assertFalse(givenAdapters[1] is Separator)
     }
+
+    @Test
+    fun shouldHaveSeparatorBetweenNotFilledInDayAndFilledIn() {
+        val givenAdapters = addSeparators(listOf(newDayWithoutReports(), newDayWithHourlyReports()))
+        assertTrue(givenAdapters[1] is Separator)
+    }
+
+    private fun newDayWithoutReports() = DayWithoutReports(0, "", "", false, false)
+
+    private fun newDayWithHourlyReports() = DayWithHourlyReports(0, "", "", listOf(newRegularHourlyReport()), false, 1.0)
 }

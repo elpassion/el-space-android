95d088032c5e231e5af4da54aa590d218852ac6e
macPry <maciej.przybyl@elpassion.pl>
2017-07-26 12:44:15 +0200

    #145 Have empty on first position

--- el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/AddSeparator.kt
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/AddSeparator.kt
@@ -5,6 +5,7 @@ import pl.elpassion.elspace.hub.report.RegularHourlyReport
 
 
 fun addSeparators(items: List<AdapterItem>) = mutableListOf<AdapterItem>().apply {
+    add(Empty())
     items.forEachIndexed { i, currentItem ->
         val previousItem = items.getOrNull(i - 1)
         if (previousItem != null && (previousItem is DayWithoutReports && previousItem.isWeekend && currentItem is DayWithoutReports && currentItem.isWeekend)) {

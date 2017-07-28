2577eb6dc54cad89db227895a5a850e85043c1e1
macPry <maciej.przybyl@elpassion.pl>
2017-07-28 17:12:46 +0200

    #148 Call api with really given message

--- el-debate/src/test/java/pl/elpassion/elspace/debate/chat/ChatControllerTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/ChatControllerTest.kt
@@ -11,20 +11,27 @@ class ChatControllerTest {
     val controller = ChatController(api)
 
     @Test
-    fun shouldCallApiWhenSendMessage() {
-        controller.sendMessage()
-        verify(api).sendMessage()
+    fun shouldCallApiWithGivenMessage() {
+        controller.sendMessage("Message")
+        verify(api).sendMessage("Message")
+    }
+
+    @Test
+    fun shouldCallApiWithReallyGivenMessage() {
+        val message = "SomeMessage"
+        controller.sendMessage(message)
+        verify(api).sendMessage(message)
     }
 }
 
 interface DebateChat {
     interface Api {
-        fun sendMessage()
+        fun sendMessage(message: String)
     }
 }
 
 class ChatController(val api: DebateChat.Api) {
-    fun sendMessage() {
-        api.sendMessage()
+    fun sendMessage(message: String) {
+        api.sendMessage(message)
     }
 }

be12ac4ca71c208d6d5c22e7c4d1ef247c660513
macPry <maciej.przybyl@elpassion.pl>
2017-08-03 14:35:44 +0200

    #148 [refactor] Reformat code

--- el-debate/src/main/java/pl/elpassion/elspace/debate/chat/Comment.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/chat/Comment.kt
@@ -1,3 +1,7 @@
 package pl.elpassion.elspace.debate.chat
 
-class Comment(val initials: String, val backgroundColor: Int, val name: String, val message: String, val isPostedByLoggedUser: Boolean)
+data class Comment(val initials: String,
+                   val backgroundColor: Int,
+                   val name: String,
+                   val message: String,
+                   val isPostedByLoggedUser: Boolean)
--- el-debate/src/main/java/pl/elpassion/elspace/debate/chat/CommentToSend.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/chat/CommentToSend.kt
@@ -1,4 +1,6 @@
 package pl.elpassion.elspace.debate.chat
 
-
-data class CommentToSend(val token: String, val message: String, val firstName: String, val lastName: String)
+data class CommentToSend(val token: String,
+                         val message: String,
+                         val firstName: String,
+                         val lastName: String)

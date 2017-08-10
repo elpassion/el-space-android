f108b929a2f981e4a1809eeb9d2f8f0cf49e1367
macPry <maciej.przybyl@elpassion.pl>
2017-08-10 12:43:31 +0200

    #148 Call api with real token

--- el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
@@ -2,6 +2,7 @@ package pl.elpassion.elspace.debate.chat
 
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
 import io.reactivex.subjects.SingleSubject
@@ -11,22 +12,30 @@ import pl.elpassion.elspace.dabate.chat.createComment
 
 class DebateChatServiceTest {
 
+    private val commentsFromApi: ArrayList<Comment> = arrayListOf(createComment(name = "FirstTestName"), createComment(name = "TestName"))
+    private val commentsFromApiSubject = SingleSubject.create<List<Comment>>()
+    private val api = mock<DebateChat.Api>().apply {
+        whenever(comment(any())).thenReturn(commentsFromApiSubject)
+    }
+    private val debateChatServiceImpl = DebateChatServiceImpl(api)
+
     @Test
     fun shouldReturnCommentsReceivedFromApi() {
-        val commentsFromApi: ArrayList<Comment> = arrayListOf(createComment(name = "FirstTestName"), createComment(name = "TestName"))
-        val commentsFromApiSubject = SingleSubject.create<List<Comment>>()
-        val api = mock<DebateChat.Api>().apply {
-            whenever(comment(any())).thenReturn(commentsFromApiSubject)
-        }
-        DebateChatServiceImpl(api)
-                .commentsObservable()
+        debateChatServiceImpl
+                .commentsObservable("token")
                 .test()
                 .apply { commentsFromApiSubject.onSuccess(commentsFromApi) }
                 .assertValues(*commentsFromApi.toTypedArray())
+    }
+
+    @Test
+    fun shouldCallApiWithRealToken() {
+        debateChatServiceImpl.commentsObservable("someToken")
+        verify(api).comment("someToken")
     }
 }
 
 class DebateChatServiceImpl(private val api: DebateChat.Api) {
 
-    fun commentsObservable(): Observable<Comment> = api.comment("").flattenAsObservable { it }
+    fun commentsObservable(token: String): Observable<Comment> = api.comment(token).flattenAsObservable { it }
 }

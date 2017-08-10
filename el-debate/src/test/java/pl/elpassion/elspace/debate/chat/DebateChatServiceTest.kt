a6ce9af28786a9772318c21a3e2fa03eb7ed32ac
macPry <maciej.przybyl@elpassion.pl>
2017-08-10 11:30:39 +0200

    #148 [refactor] Remove unused val

---
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
@@ -0,0 +1,16 @@
+package pl.elpassion.elspace.debate.chat
+
+import org.junit.Test
+
+
+class DebateChatServiceTest {
+
+    @Test
+    fun initTest() {
+        DebateChatServiceImpl()
+    }
+}
+
+class DebateChatServiceImpl {
+
+}

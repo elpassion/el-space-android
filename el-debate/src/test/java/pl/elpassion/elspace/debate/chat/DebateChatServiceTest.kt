d69c5b0587ae83bbedefc96cbdffe9e093bf580e
macPry <maciej.przybyl@elpassion.pl>
2017-08-10 11:40:44 +0200

    #148 Call api on subscribe to service comments observable

--- el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
@@ -1,16 +1,26 @@
 package pl.elpassion.elspace.debate.chat
 
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.Observable
 import org.junit.Test
 
 
 class DebateChatServiceTest {
 
     @Test
-    fun initTest() {
-        DebateChatServiceImpl()
+    fun shouldCallApiOnSubscribeToServiceCommentsObservable() {
+        val api = mock<DebateChat.Api>()
+        DebateChatServiceImpl(api).commentsObservable().subscribe()
+        verify(api).comment(any())
     }
 }
 
-class DebateChatServiceImpl {
+class DebateChatServiceImpl(private val api: DebateChat.Api) {
 
+    fun commentsObservable(): Observable<Comment> {
+        api.comment("...")
+        return Observable.error(RuntimeException())
+    }
 }

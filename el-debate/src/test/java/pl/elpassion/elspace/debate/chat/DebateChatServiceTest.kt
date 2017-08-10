1b5ac2a620be0ac9c3181edfaced3d581a48a0c9
macPry <maciej.przybyl@elpassion.pl>
2017-08-10 11:58:15 +0200

    #148 Return comments received from api

--- el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/DebateChatServiceTest.kt
@@ -2,25 +2,31 @@ package pl.elpassion.elspace.debate.chat
 
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
+import io.reactivex.subjects.SingleSubject
 import org.junit.Test
+import pl.elpassion.elspace.dabate.chat.createComment
 
 
 class DebateChatServiceTest {
 
     @Test
-    fun shouldCallApiOnSubscribeToServiceCommentsObservable() {
-        val api = mock<DebateChat.Api>()
-        DebateChatServiceImpl(api).commentsObservable().subscribe()
-        verify(api).comment(any())
+    fun shouldReturnCommentsReceivedFromApi() {
+        val commentsFromApi: ArrayList<Comment> = arrayListOf(createComment(name = "FirstTestName"), createComment(name = "TestName"))
+        val commentsFromApiSubject = SingleSubject.create<List<Comment>>()
+        val api = mock<DebateChat.Api>().apply {
+            whenever(comment(any())).thenReturn(commentsFromApiSubject)
+        }
+        DebateChatServiceImpl(api)
+                .commentsObservable()
+                .test()
+                .apply { commentsFromApiSubject.onSuccess(commentsFromApi) }
+                .assertValues(*commentsFromApi.toTypedArray())
     }
 }
 
 class DebateChatServiceImpl(private val api: DebateChat.Api) {
 
-    fun commentsObservable(): Observable<Comment> {
-        api.comment("...")
-        return Observable.error(RuntimeException())
-    }
+    fun commentsObservable(): Observable<Comment> = api.comment("").flattenAsObservable { it }
 }
--- el-debate/src/testCommon/java/pl/elpassion/elspace/dabate/chat/CommentFactory.kt
+++ el-debate/src/testCommon/java/pl/elpassion/elspace/dabate/chat/CommentFactory.kt
@@ -3,4 +3,4 @@ package pl.elpassion.elspace.dabate.chat
 import pl.elpassion.elspace.debate.chat.Comment
 
 fun createCommentByLoggedUser() = Comment(name = "First Last", initials = "FO", backgroundColor = 0xFFFF0000.toInt(), message = "Message", isPostedByLoggedUser = true)
-fun createComment() = Comment(name = "OtherFirst OtherLast", initials = "WX", backgroundColor = 0xFF0000FF.toInt(), message = "OtherMessage", isPostedByLoggedUser = false)
+fun createComment(name: String = "OtherFirst OtherLast") = Comment(name = name, initials = "WX", backgroundColor = 0xFF0000FF.toInt(), message = "OtherMessage", isPostedByLoggedUser = false)

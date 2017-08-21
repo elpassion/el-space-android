90888bcfc4b67ede533353d47f6c0e73e2e6958b
macPry <maciej.przybyl@elpassion.pl>
2017-08-21 12:29:26 +0200

    #148 Throw socket exception when pusher is disconnected

--- el-debate/src/main/java/pl/elpassion/elspace/debate/chat/DebateChatSocketImpl.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/debate/chat/DebateChatSocketImpl.kt
@@ -5,44 +5,48 @@ import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.pusher.client.Pusher
 import com.pusher.client.PusherOptions
+import com.pusher.client.channel.Channel
 import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionState
 import com.pusher.client.connection.ConnectionStateChange
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
 import pl.elpassion.BuildConfig
+import java.net.SocketException
 
 class DebateChatSocketImpl : DebateChat.Socket {
 
     override fun commentsObservable(debateCode: String): Observable<Comment> = Observable.create<Comment> { emitter: ObservableEmitter<Comment> ->
         val pusher = Pusher("###", PusherOptions().setCluster("eu"))
         connectPusher(pusher, emitter)
+        val channel = pusher.subscribe("my-channel")
+        bindToChannel(channel, emitter)
         emitter.setCancellable { pusher.disconnect() }
     }
 
     private fun connectPusher(pusher: Pusher, emitter: ObservableEmitter<Comment>) {
-        if (BuildConfig.DEBUG) connectPusherDebug(pusher) else pusher.connect()
-        bindToChannel(pusher, emitter)
-    }
-
-    private fun connectPusherDebug(pusher: Pusher) {
         pusher.connect(object : ConnectionEventListener {
             override fun onConnectionStateChange(connectionStateChange: ConnectionStateChange?) {
-                Log.i("PUSHER ConnectionState", connectionStateChange?.currentState?.name)
+                when {
+                    (BuildConfig.DEBUG) -> Log.i("PUSHER ConnectionState", connectionStateChange?.currentState?.name)
+                    connectionStateChange?.currentState == ConnectionState.DISCONNECTED -> emitter.onError(SocketException())
+                }
             }
 
             override fun onError(p0: String?, p1: String?, exception: Exception?) {
-                Log.e("PUSHER onError", "p0: $p0, p1: $p1, p2: ${exception?.message}")
+                if (BuildConfig.DEBUG) {
+                    Log.e("PUSHER onError", "p0: $p0, p1: $p1, p2: ${exception?.message}")
+                }
             }
         })
     }
 
-    private fun bindToChannel(pusher: Pusher, emitter: ObservableEmitter<Comment>) {
-        val channel = pusher.subscribe("my-channel")
+    private fun bindToChannel(channel: Channel, emitter: ObservableEmitter<Comment>) {
         channel.bind("my-event", { channelName, eventName, data ->
-            if (BuildConfig.DEBUG) {
-                Log.i("PUSHER onEvent", "channelName: $channelName, eventName: $eventName, data: $data")
+            when {
+                (BuildConfig.DEBUG) -> Log.i("PUSHER onEvent", "channelName: $channelName, eventName: $eventName, data: $data")
+                (data != null) -> emitter.onNext(createComment(data))
             }
-            if (data != null) emitter.onNext(createComment(data))
         })
     }
 

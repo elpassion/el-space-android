e33543b8ad47aee9650991e1582b7195e61b97bb
macPry <maciej.przybyl@elpassion.pl>
2017-08-28 18:09:48 +0200

    #148 [refactor] Move decryption to separate file

--- scripts/decrypt-el-space-release.sh
+++ scripts/decrypt-el-space-release.sh
@@ -3,4 +3,3 @@ openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release
 openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-signing-credentials.gradle.enc -out el-space-app/src/release/google-signing-credentials.gradle
 openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/google-play-api-key.json.enc -out el-space-app/src/release/google-play-api-key.json
 openssl aes-256-cbc -d -k "$FABRIC_GOOGLE_PASSWORD" -in el-space-app/src/release/release-keystore.jks.enc -out el-space-app/src/release/release-keystore.jks
-openssl aes-256-cbc -d -k "$PUSHER_PSW" -in el-space-android/pusher.properties.enc -out el-space-android/pusher.properties

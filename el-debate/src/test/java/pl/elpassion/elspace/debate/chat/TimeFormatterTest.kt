ff0928ec49b79ddf9483738df785b42a9931667b
macPry <maciej.przybyl@elpassion.pl>
2017-08-28 16:25:24 +0200

    #148 [refactor] Use assertEquals

--- el-debate/src/test/java/pl/elpassion/elspace/debate/chat/TimeFormatterTest.kt
+++ el-debate/src/test/java/pl/elpassion/elspace/debate/chat/TimeFormatterTest.kt
@@ -1,6 +1,6 @@
 package pl.elpassion.elspace.debate.chat
 
-import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.util.*
 
@@ -9,18 +9,18 @@ class TimeFormatterTest {
     @Test
     fun shouldReturnCorrectTime() {
         val time = 50000000L.getTime(TimeZone.getTimeZone("GMT"))
-        assertTrue(time == "13:53")
+        assertEquals(time, "13:53")
     }
 
     @Test
     fun shouldReturnCorrectTimeForDifferentTimeZone() {
         val time = 50000000L.getTime(TimeZone.getTimeZone("Europe/Warsaw"))
-        assertTrue(time == "14:53")
+        assertEquals(time, "14:53")
     }
 
     @Test
     fun shouldReturnCorrectTimeForModernTimestamp() {
         val time = 1503413839000L.getTime(TimeZone.getTimeZone("GMT"))
-        assertTrue(time == "14:57")
+        assertEquals(time, "14:57")
     }
 }

a03dd9d1affd450a8ddd2dbbfadbd99851545577
Dominik <dominik.malantowicz@gmail.com>
2017-09-12 15:37:34 +0200

    ReportListModel init test

---
+++ el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
@@ -0,0 +1,18 @@
+package pl.elpassion.elspace.hub.report.list
+
+import pl.elpassion.elspace.common.TreeSpec
+
+class ReportListModelTest : TreeSpec() {
+
+    init {
+
+        "Init test" > {
+            ReportListModel()
+        }
+    }
+
+
+
+}
+
+class ReportListModel

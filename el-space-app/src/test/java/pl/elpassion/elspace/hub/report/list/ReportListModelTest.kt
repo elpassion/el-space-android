3a5cf246ec4f837aacb54abeca1095e1a301b18c
Dominik <dominik.malantowicz@gmail.com>
2017-09-13 16:33:33 +0200

    On create call service for days

--- el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
+++ el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
@@ -2,8 +2,15 @@ package pl.elpassion.elspace.hub.report.list
 
 import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jakewharton.rxrelay2.PublishRelay
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
 import pl.elpassion.elspace.common.TreeSpec
+import pl.elpassion.elspace.common.extensions.getTimeFrom
+import pl.elpassion.elspace.hub.report.list.service.ReportDayService
+import java.util.*
 
 class ReportListModelTest : TreeSpec() {
 
@@ -11,23 +18,35 @@ class ReportListModelTest : TreeSpec() {
         "Model should " {
             "on create " {
                 "propagate list of adapters" > {
-                    val model = ReportListModel()
+                    val service = mock<ReportDayService>().apply {
+                        whenever(this.createDays(any())).thenReturn(Observable.just(emptyList()))
+                    }
+                    val model = ReportListModel(service)
                     Observable.just(ReportList.Event.OnCreate).subscribe(model.events)
                     model.states
                             .test()
                             .assertValue { it.adapterItems.isEmpty() }
+                }
+
+                "call service for days" > {
+                    val service = mock<ReportDayService>().apply {
+                        whenever(this.createDays(any())).thenReturn(Observable.just(emptyList()))
+                    }
+                    val model = ReportListModel(service)
+                    model.events.accept(ReportList.Event.OnCreate)
+                    verify(service).createDays(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth())
                 }
             }
         }
     }
 }
 
-class ReportListModel {
+class ReportListModel(service: ReportDayService) {
     val states: BehaviorRelay<ReportList.UIState> = BehaviorRelay.create()
     val events: PublishRelay<ReportList.Event> = PublishRelay.create()
 
     init {
-        events
+        events.flatMap { service.createDays(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth()) }
                 .map { ReportList.UIState(emptyList()) }
                 .subscribe(states)
     }

4ad5f913c4367184acd5e43e29cb4097c0e27415
Dominik <dominik.malantowicz@gmail.com>
2017-09-15 17:09:18 +0200

    On create propagate list of adapters returned from service

--- el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
+++ el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
@@ -6,16 +6,18 @@ import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import pl.elpassion.elspace.common.TreeSpec
 import pl.elpassion.elspace.common.extensions.getTimeFrom
+import pl.elpassion.elspace.hub.report.list.adapter.Empty
 import pl.elpassion.elspace.hub.report.list.service.ReportsListAdaptersService
 import java.util.*
 
 class ReportListModelTest : TreeSpec() {
 
+    private val reportListAdaptersSubject = PublishSubject.create<List<AdapterItem>>()
     private val service = mock<ReportsListAdaptersService>().apply {
-        whenever(createReportsListAdapters(any())).thenReturn(Observable.just(emptyList()))
+        whenever(createReportsListAdapters(any())).thenReturn(reportListAdaptersSubject)
     }
     private val model = ReportListModel(service)
 
@@ -23,13 +25,16 @@ class ReportListModelTest : TreeSpec() {
         "Model should " {
             "on create " {
                 model.events.accept(ReportList.Event.OnCreate)
-                "propagate list of adapters" > {
-                    model.states.test().assertValue { it.adapterItems.isEmpty() }
+                "propagate list of adapters returned from service" > {
+                    val reportListAdapters = listOf(Empty(), Empty())
+                    reportListAdaptersSubject.onNext(reportListAdapters)
+                    model.states.test().assertValue { it.adapterItems == reportListAdapters }
                 }
-                "call service for days" > {
+                "call service for report list adapters" > {
                     verify(service).createReportsListAdapters(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth())
                 }
                 "show loader" > {
+                    reportListAdaptersSubject.onNext(emptyList())
                     model.states.test().assertValue { it.isLoaderVisible }
                 }
             }
@@ -43,7 +48,7 @@ class ReportListModel(service: ReportsListAdaptersService) {
 
     init {
         events.flatMap { service.createReportsListAdapters(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth()) }
-                .map { ReportList.UIState(adapterItems = emptyList(), isLoaderVisible = true) }
+                .map { ReportList.UIState(adapterItems = it, isLoaderVisible = true) }
                 .subscribe(states)
     }
 }

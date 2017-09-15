c6fda8618bebf745c1708f7a80f2edd65d0d12ac
Dominik <dominik.malantowicz@gmail.com>
2017-09-15 17:16:48 +0200

    Start states with predefined ui state

--- el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
+++ el-space-app/src/test/java/pl/elpassion/elspace/hub/report/list/ReportListModelTest.kt
@@ -2,6 +2,7 @@ package pl.elpassion.elspace.hub.report.list
 
 import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jakewharton.rxrelay2.PublishRelay
+import com.jakewharton.rxrelay2.Relay
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
@@ -23,6 +24,10 @@ class ReportListModelTest : TreeSpec() {
 
     init {
         "Model should " {
+            "start with predefined ui state" > {
+                model.states.test()
+                        .assertValue { it == ReportListModel.startState }
+            }
             "on create " {
                 model.events.accept(ReportList.Event.OnCreate)
                 "propagate list of adapters returned from service" > {
@@ -43,12 +48,17 @@ class ReportListModelTest : TreeSpec() {
 }
 
 class ReportListModel(service: ReportsListAdaptersService) {
-    val states: BehaviorRelay<ReportList.UIState> = BehaviorRelay.create()
+    val states: Relay<ReportList.UIState> = BehaviorRelay.create<ReportList.UIState>().apply {
+        accept(startState)
+    }
     val events: PublishRelay<ReportList.Event> = PublishRelay.create()
 
     init {
         events.flatMap { service.createReportsListAdapters(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth()) }
                 .map { ReportList.UIState(adapterItems = it, isLoaderVisible = true) }
                 .subscribe(states)
+    }
+    companion object {
+        val startState = ReportList.UIState(emptyList(), false)
     }
 }

829510cc3ab355144e7274fe2c03510c719bf47c
Dominik <dominik.malantowicz@gmail.com>
2017-09-20 14:09:44 +0200

    [refactoring] Extract hard to read code to self describing fields/methods

--- el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/ReportListModel.kt
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/ReportListModel.kt
@@ -11,26 +11,29 @@ import pl.elpassion.elspace.hub.report.list.service.ReportsListAdaptersService
 import java.util.*
 
 class ReportListModel(service: ReportsListAdaptersService) {
+
     val states: Relay<ReportList.UIState> = BehaviorRelay.create<ReportList.UIState>().apply {
         accept(startState)
     }
+
     val events: PublishRelay<ReportList.Event> = PublishRelay.create()
 
+    private val handleOnCreateEvent = events.ofType(ReportList.Event.OnCreate::class.java)
+            .mapToLastFrom(states)
+            .switchMap { state ->
+                showLoader(state)
+                        .concatWith(callServiceForAdapterItems)
+            }
+
+    private fun showLoader(state: ReportList.UIState) =
+            Observable.just(state.copy(isLoaderVisible = true))
+
+    private val callServiceForAdapterItems = service.createReportsListAdapters(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth())
+            .mapToWithLastFrom(states) { state -> state.copy(adapterItems = this, isLoaderVisible = false) }
+
     init {
-        handleFetchingReportListAdapters(service).subscribe(states)
+        handleOnCreateEvent.subscribe(states)
     }
-
-    private fun handleFetchingReportListAdapters(service: ReportsListAdaptersService) =
-            events.ofType(ReportList.Event.OnCreate::class.java)
-                    .mapToLastFrom(states)
-                    .switchMap { state ->
-                        Observable.just(state.copy(isLoaderVisible = true))
-                                .concatWith(
-                                        service.createReportsListAdapters(yearMonth = getTimeFrom(year = 2016, month = Calendar.JUNE, day = 1).toYearMonth())
-                                                .mapToWithLastFrom(states) { state -> state.copy(adapterItems = this, isLoaderVisible = false) }
-                                )
-
-                    }
 
     companion object {
         val startState = ReportList.UIState(emptyList(), false)

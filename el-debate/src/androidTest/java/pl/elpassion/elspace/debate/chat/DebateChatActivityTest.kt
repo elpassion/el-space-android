a5d0a439c623374940598e9281dd97085c04ed64
macPry <maciej.przybyl@elpassion.pl>
2017-09-25 12:12:02 +0200

    #158 [fix] Confirm one swipe

--- el-debate/src/androidTest/java/pl/elpassion/elspace/debate/chat/DebateChatActivityTest.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/debate/chat/DebateChatActivityTest.kt
@@ -590,7 +590,6 @@ class DebateChatActivityTest {
 
     private fun swipeDown() {
         Espresso.closeSoftKeyboard()
-        //onId(R.id.debateChatCommentsContainer).swipeDown()
         onId(R.id.debateChatCommentsContainer).swipeDown()
     }
 }

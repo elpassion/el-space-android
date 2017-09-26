289dff2f88e1fe23f765002ee94e3ef31f60c7dc
Dominik <dominik.malantowicz@gmail.com>
2017-09-26 17:04:48 +0200

    Do not filter out Empty adapterItems

--- el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/service/DayFilterImpl.kt
+++ el-space-app/src/main/java/pl/elpassion/elspace/hub/report/list/service/DayFilterImpl.kt
@@ -2,9 +2,11 @@ package pl.elpassion.elspace.hub.report.list.service
 
 import pl.elpassion.elspace.hub.report.list.AdapterItem
 import pl.elpassion.elspace.hub.report.list.DayWithoutReports
+import pl.elpassion.elspace.hub.report.list.adapter.Empty
+import pl.elpassion.elspace.hub.report.list.shouldHaveReports
 
 class DayFilterImpl : DayFilter {
 
-    override fun fetchFilteredDays(days: List<AdapterItem>) = days.filter { it is DayWithoutReports && it.hasPassed && !it.isWeekend }
+    override fun fetchFilteredDays(days: List<AdapterItem>) = days.filter { it is DayWithoutReports && it.shouldHaveReports() || it is Empty }
 
 }

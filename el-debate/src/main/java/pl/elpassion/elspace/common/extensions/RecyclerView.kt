aff1ccdb56a6c5c0bdd8dc25631a32e3beaf352c
macPry <maciej.przybyl@elpassion.pl>
2017-10-04 13:09:34 +0200

    #158 [refactor] Remove unnecessary run

--- el-debate/src/main/java/pl/elpassion/elspace/common/extensions/RecyclerView.kt
+++ el-debate/src/main/java/pl/elpassion/elspace/common/extensions/RecyclerView.kt
@@ -3,17 +3,14 @@ package pl.elpassion.elspace.common.extensions
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 
-
 fun RecyclerView.getVisibleItemsPositions(): IntRange {
-    run {
-        (layoutManager as LinearLayoutManager).run {
-            val firstVisibleItemPosition = findFirstCompletelyVisibleItemPosition()
-            val lastVisibleItemPosition = findLastCompletelyVisibleItemPosition()
-            return if (firstVisibleItemPosition > -1) {
-                firstVisibleItemPosition..lastVisibleItemPosition
-            } else {
-                IntRange.EMPTY
-            }
+    (layoutManager as LinearLayoutManager).run {
+        val firstVisibleItemPosition = findFirstCompletelyVisibleItemPosition()
+        val lastVisibleItemPosition = findLastCompletelyVisibleItemPosition()
+        return if (firstVisibleItemPosition > -1) {
+            firstVisibleItemPosition..lastVisibleItemPosition
+        } else {
+            IntRange.EMPTY
         }
     }
 }

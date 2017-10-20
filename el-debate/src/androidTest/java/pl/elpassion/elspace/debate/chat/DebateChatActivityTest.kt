0711fa20ac4dc73453d037bbb6490bf320b808cc
macPry <maciej.przybyl@elpassion.pl>
2017-10-20 16:17:45 +0200

    [fix] Remove unnecessary swipe

--- el-debate/src/androidTest/java/pl/elpassion/elspace/debate/chat/DebateChatActivityTest.kt
+++ el-debate/src/androidTest/java/pl/elpassion/elspace/debate/chat/DebateChatActivityTest.kt
@@ -749,6 +749,5 @@ class DebateChatActivityTest {
     private fun swipeDown() {
         Espresso.closeSoftKeyboard()
         onId(R.id.debateChatCommentsContainer).swipeDown()
-        onId(R.id.debateChatCommentsContainer).swipeDown()
     }
 }
